349754535e5c749d8078bdca7c37ba7d3f49eed8
Damian Minkov <damencho@jitsi.org>
2013-03-21 15:47:50 +0000

    Patch provided by Pavel Tankov on dev (March 21, 2013, subject: "[jitsi-dev] [PATCH] Adding creation of artifacts for all of the supported OSes").

---
+++ resources/install/linux/jvb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ "$1" == "--help"  || $# -lt 1 ]]; then
+    echo "Usage:"
+    echo "$0 secret [port] [host] [minPort] [maxPort]"
+    echo
+    echo "Default values are:"
+    echo port="5275"
+    echo host="localhost"
+    echo minPort="10000"
+    echo maxPort="20000"
+    exit 1
+fi
+
+secret="$1"
+port="$2"
+host="$3"
+minPort="$4"
+maxPort="$5"
+
+[ $port ] || port="5275"
+[ $host ] || host="localhost"
+[ $minPort ] || minPort="10000"
+[ $maxPort ] || maxPort="20000"
+
+mainClass="org.jitsi.videobridge.Main"
+
+cp=$(JARS=(jitsi-videobridge.jar lib/*.jar); IFS=:; echo "${JARS[*]}")
+
+java -cp $cp $mainClass --secret="$secret" --port="$port" --host="$host" --min-port="$minPort" --max-port="$maxPort"
+
