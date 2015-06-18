ec9e05b05ae3203d1244c7b30db71432a0a05b6b
George Politis <gp@jitsi.org>
2015-06-18 16:57:05 +0200

    Updates the license headers.

--- openfire/src/web/jitsi-videobridge.jsp
+++ openfire/src/web/jitsi-videobridge.jsp
@@ -1,8 +1,17 @@
 <%--
- - Jitsi VideoBridge, OpenSource video conferencing.
- -
- - Distributable under LGPL license.
- - See terms of license at gnu.org.
+ _ Copyright @ 2015 Atlassian Pty Ltd
+ _
+ _ Licensed under the Apache License, Version 2.0 (the "License");
+ _ you may not use this file except in compliance with the License.
+ _ You may obtain a copy of the License at
+ _
+ _     http://www.apache.org/licenses/LICENSE-2.0
+ _
+ _ Unless required by applicable law or agreed to in writing, software
+ _ distributed under the License is distributed on an "AS IS" BASIS,
+ _ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ _ See the License for the specific language governing permissions and
+ _ limitations under the License.
 --%>
 <%@ page import="org.jitsi.videobridge.openfire.*" %>
 <%@ page import="org.jivesoftware.openfire.*" %>
--- src/org/jitsi/videobridge/transform/RTCPTransformEngine.java
+++ src/org/jitsi/videobridge/transform/RTCPTransformEngine.java
@@ -1,20 +1,17 @@
 package org.jitsi.videobridge.transform;
-
-import net.sf.fmj.media.rtp.*;
-import net.sf.fmj.media.rtp.util.*;
-import org.jitsi.impl.neomedia.*;
-import org.jitsi.impl.neomedia.rtcp.*;
-import org.jitsi.impl.neomedia.transform.*;
-import org.jitsi.service.neomedia.*;
-import org.jitsi.util.*;
-
-/**
- * A <tt>TransformEngine</tt> implementation which parses RTCP packets and
- * transforms them using a transformer for <tt>RTCPCompoundPacket</tt>s.
- * This is similar to (and based on) <tt>libjitsi</tt>'s
- * <tt>RTCPTerminationTransformEngine</tt> but is not connected with a
- * <tt>MediaStream</tt>.
- * @author Boris Grozev
+ * Copyright @ 2015 Atlassian Pty Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class RTCPTransformEngine
     extends SinglePacketTransformer
