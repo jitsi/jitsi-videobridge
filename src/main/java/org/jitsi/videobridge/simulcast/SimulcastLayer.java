55a985045d58171ae21b745d3cbededddf87fd69
Lyubomir Marinov <lyubomir.marinov@jitsi.org>
2015-10-23 12:01:56 -0500

    Fixes warnings.

--- src/main/java/org/jitsi/videobridge/simulcast/SimulcastLayer.java
+++ src/main/java/org/jitsi/videobridge/simulcast/SimulcastLayer.java
@@ -19,7 +19,6 @@ import java.util.*;
 import java.util.concurrent.*;
 
 import org.jitsi.impl.neomedia.*;
-import org.jitsi.impl.neomedia.rtp.remotebitrateestimator.*;
 import org.jitsi.util.*;
 import org.jitsi.util.event.*;
 
