e944d05a75234b5156bcacb176025fe7c38705e2
George Politis <gp@jitsi.org>
2015-11-02 22:28:44 +0000

    Updates the dump_simulcast.groovy script.

--- resources/scripts/dump_simulcast.groovy
+++ resources/scripts/dump_simulcast.groovy
@@ -2,10 +2,11 @@ state.videobridge.getConferences().each() { conference ->
     // video content
     conference.getContents()[1].getChannels().each() { channel ->
         println "Endpoint: $channel.endpoint.id"
-        println "Sending: $channel.transformEngine.simulcastEngine.simulcastReceiver.simulcastLayers"
         channel.transformEngine.simulcastEngine.simulcastSenderManager.senders.each() { receiver, sender ->
-            def layer = sender.sendMode.weakCurrent.get();
-            println "Receiving: $layer"
+            println "Receiving from the bridge: $sender"
+            println "The bridge receives : $receiver"
         }
+
+        print "Rewriting: $channel.stream.ssrcRewritingEngine"
     }
 }
