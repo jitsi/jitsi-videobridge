8467a5d0e71d2bab808ab52961f38842b36f859b
George Politis <gp@jitsi.org>
2015-12-01 11:33:38 -0600

    Revert "Adds 2 groovy scripts."
    This reverts commit 51f84fac59f980f35c9dfce58d6126b043dfd23d.

--- scripts/max_bitrate.groovy
+++
@@ -1,7 +0,0 @@
-state.videobridge.getConferences().each() { conference ->
-    // video content
-    conference.getContents()[1].getChannels().each() { channel ->
-        def strategy = new org.jitsi.impl.neomedia.rtcp.termination.strategies.MaxThroughputRTCPTerminationStrategy();
-        channel.getStream().setRTCPTerminationStrategy(strategy)
-    }
-}
