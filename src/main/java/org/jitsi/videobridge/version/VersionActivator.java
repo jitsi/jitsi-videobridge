33779848bbb0a8b3e80ab0c4448d534604000b69
paweldomas <pawel.domas@jitsi.org>
2016-02-29 16:08:00 -0600

    Moves VersionService implementation to Jicoco. Unbinds temporarily jicoco version(as it's unknown).

--- src/main/java/org/jitsi/videobridge/version/VersionActivator.java
+++ src/main/java/org/jitsi/videobridge/version/VersionActivator.java
@@ -15,114 +15,30 @@
  */
 package org.jitsi.videobridge.version;
 
-import net.java.sip.communicator.util.*;
-import org.jitsi.service.configuration.*;
-import org.jitsi.service.version.*;
-import org.jitsi.service.version.Version;
-import org.osgi.framework.*;
+import org.jitsi.version.*;
 
 /**
- * The entry point to the {@code VersionService} implementation. We register the
- * {@code VersionServiceImpl} instance on the OSGi bus.
+ * Extends {@link AbstractVersionActivator} in order to provider the
+ * {@code VersionService} implementation for the Jitsi Videobridge.
  *
- * @author Emil Ivov
- * @author George Politis
+ * @author Pawel Domas
  */
 public class VersionActivator
-    implements BundleActivator
+    extends AbstractVersionActivator
 {
     /**
-     * The <tt>Logger</tt> used by this <tt>VersionActivator</tt> instance for
-     * logging output.
+     * The {@code CurrentVersionImpl} instance describing the current version
+     * of Jitsi Videobridge.
      */
-    private final Logger logger = Logger.getLogger(VersionActivator.class);
+    private static final CurrentVersionImpl CURRENT_VERSION
+        = new CurrentVersionImpl();
 
     /**
-     * The OSGi <tt>BundleContext</tt>.
+     * {@inheritDoc}
      */
-    private static BundleContext bundleContext;
-
-    /**
-     * Called when this bundle is started so the Framework can perform the
-     * bundle-specific activities necessary to start this bundle.
-     *
-     * @param context The execution context of the bundle being started.
-     * @throws Exception If this method throws an exception, this bundle is
-     * marked as stopped and the Framework will remove this bundle's listeners,
-     * unregister all services registered by this bundle, and release all
-     * services used by this bundle.
-     */
-    public void start(BundleContext context) throws Exception
+    @Override
+    protected CurrentVersion getCurrentVersion()
     {
-        if (logger.isDebugEnabled())
-            logger.debug("Started.");
-
-        VersionActivator.bundleContext = context;
-
-        context.registerService(
-                VersionService.class.getName(),
-                new VersionServiceImpl(),
-                null);
-
-        if (logger.isDebugEnabled())
-            logger.debug("Jitsi Videobridge Version Service ... [REGISTERED]");
-
-        Version version = VersionImpl.currentVersion();
-        String applicationName = version.getApplicationName();
-        String versionString = version.toString();
-
-        if (logger.isInfoEnabled())
-        {
-            logger.info(
-                    "Jitsi Videobridge Version: " + applicationName + " "
-                        + versionString);
-        }
-
-        //register properties for those that would like to use them
-        ConfigurationService cfg = getConfigurationService();
-
-        cfg.setProperty(Version.PNAME_APPLICATION_NAME, applicationName, true);
-        cfg.setProperty(Version.PNAME_APPLICATION_VERSION, versionString, true);
-    }
-
-    /**
-     * Gets a <tt>ConfigurationService</tt> implementation currently registered
-     * in the <tt>BundleContext</tt> in which this bundle has been started or
-     * <tt>null</tt> if no such implementation was found.
-     *
-     * @return a <tt>ConfigurationService</tt> implementation currently
-     * registered in the <tt>BundleContext</tt> in which this bundle has been
-     * started or <tt>null</tt> if no such implementation was found
-     */
-    private static ConfigurationService getConfigurationService()
-    {
-        return
-            ServiceUtils.getService(bundleContext, ConfigurationService.class);
-    }
-
-    /**
-     * Gets the <tt>BundleContext</tt> instance within which this bundle has
-     * been started.
-     *
-     * @return the <tt>BundleContext</tt> instance within which this bundle has
-     * been started
-     */
-    public static BundleContext getBundleContext()
-    {
-        return bundleContext;
-    }
-
-    /**
-     * Called when this bundle is stopped so the Framework can perform the
-     * bundle-specific activities necessary to stop the bundle.
-     *
-     * @param context The execution context of the bundle being stopped.
-     * @throws Exception If this method throws an exception, the bundle is still
-     * marked as stopped, and the Framework will remove the bundle's listeners,
-     * unregister all services registered by the bundle, and release all
-     * services used by the bundle.
-     */
-    public void stop(BundleContext context) throws Exception
-    {
+        return CURRENT_VERSION;
     }
 }
