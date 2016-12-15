420f32660c0340d19d28131c4ba19773297a06ea
Boris Grozev <boris@jitsi.org>
2016-12-15 16:51:20 -0600

    style: Remove unused imports.

--- src/main/java/org/jitsi/videobridge/VideoChannel.java
+++ src/main/java/org/jitsi/videobridge/VideoChannel.java
@@ -29,12 +29,10 @@ import net.java.sip.communicator.impl.protocol.jabber.extensions.jingle.*;
 import org.ice4j.util.*;
 import org.jitsi.impl.neomedia.*;
 import org.jitsi.impl.neomedia.rtcp.*;
-import org.jitsi.impl.neomedia.rtcp.termination.strategies.*;
 import org.jitsi.impl.neomedia.transform.*;
 import org.jitsi.service.configuration.*;
 import org.jitsi.service.neomedia.*;
 import org.jitsi.service.neomedia.codec.*;
-import org.jitsi.service.neomedia.format.*;
 import org.jitsi.service.neomedia.rtp.*;
 import org.jitsi.util.*;
 import org.jitsi.util.Logger; // Disambiguation.
