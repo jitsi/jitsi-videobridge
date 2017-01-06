c4281df8cf485578019e3b1316cb99f4349c1ca5
George Politis <gp@jitsi.org>
2017-01-06 09:32:52 -0600

    style: Merges the adaptivity package into the ratecontrol package.

--- src/main/java/org/jitsi/videobridge/ratecontrol/MediaStreamTrackFactory.java
+++ src/main/java/org/jitsi/videobridge/ratecontrol/MediaStreamTrackFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jitsi.videobridge.adaptivity;
+package org.jitsi.videobridge.ratecontrol;
 
 import net.java.sip.communicator.impl.protocol.jabber.extensions.colibri.*;
 import net.java.sip.communicator.impl.protocol.jabber.extensions.jingle.*;
