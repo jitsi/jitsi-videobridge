jitsi/jitsi-videobridge

commit 59a9ae0b997c5298d6cba1b178aceb679d2a064b

ref: Disable TL0PICIDX rewriting, when Picture ID rewriting is disabled.

src/main/java/org/jitsi/videobridge/cc/SimulcastController.java
@@ -871,7 +871,7 @@ public class SimulcastController
                             tsTranslation = maxSentFrame.tsTranslation;
                         }
 
-                        int dstPictureID = -1;
+                        int dstPictureID = -1, dstTL0PICIDX = -1;
                         if (ENABLE_VP8_PICID_REWRITING)
                         {
                             byte vp8PT = bitrateController.getVideoChannel()
@@ -907,26 +907,20 @@ public class SimulcastController
                                     logger.warn("A jump was detected in the picture IDs.");
                                 }
                             }
-                        }
 
+                            int tid = ((MediaStreamImpl) bitrateController
+                                .getVideoChannel().getStream())
+                                .getTemporalID(buf, off, len);
 
-                        int tid = ((MediaStreamImpl) bitrateController
-                            .getVideoChannel().getStream())
-                            .getTemporalID(buf, off, len);
-
-                        int dstTL0PICIDX;
-                        if (tid > -1)
-                        {
-                            if (tid == 0)
+                            if (tid > -1)
                             {
-                                tl0PicIdx++;
-                            }
+                                if (tid == 0)
+                                {
+                                    tl0PicIdx++;
+                                }
 
-                            dstTL0PICIDX = tl0Idx;
-                        }
-                        else
-                        {
-                            dstTL0PICIDX = -1;
+                                dstTL0PICIDX = tl0Idx;
+                            }
                         }
 
                         destFrame = seenFrameAllocator.getOrCreate();

commit 09506694136f7ddc0d536c90603e6d9b9bf46340

fix: Use correct value for TL0PICIDX.
The issue was introduced with 59a9ae0b997c5298d6cba1b178aceb679d2a064b.

src/main/java/org/jitsi/videobridge/cc/SimulcastController.java
@@ -416,8 +416,6 @@ public class SimulcastController
             return pktIn;
         }
 
-        BitstreamController bitstreamController = this.bitstreamController;
-
         // Drop SRs from other streams.
         boolean removed = false;
         RTCPIterator it = new RTCPIterator(pktIn);
@@ -919,7 +917,7 @@ public class SimulcastController
                                     tl0PicIdx++;
                                 }
 
-                                dstTL0PICIDX = tl0Idx;
+                                dstTL0PICIDX = tl0PicIdx;
                             }
                         }
 