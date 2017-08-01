a896203898b394c3671086e48f09bc84793a609f
brian baldino <bbaldino@gmail.com>
2017-08-01 12:54:53 -0700

    [squash] remove more unused methods that took in (buf, off, len)

--- src/main/java/org/jitsi/videobridge/cc/SimulcastController.java
+++ src/main/java/org/jitsi/videobridge/cc/SimulcastController.java
@@ -189,7 +189,7 @@ public class SimulcastController
         assert sourceTrack != null;
         FrameDesc sourceFrameDesc =
             sourceTrack.findFrameDesc(pkt.getSSRCAsLong(), pkt.getTimestamp());
-        
+
         if (sourceFrameDesc == null)
         {
             return false;
@@ -943,29 +943,6 @@ public class SimulcastController
         }
 
         /**
-         * Defines an RTP packet filter that controls which packets to be written
-         * into some arbitrary target/receiver that owns this
-         * {@link BitstreamController}.
-         *
-         * @param sourceFrameDesc the {@link FrameDesc} that the RTP packet belongs
-         * to.
-         * @param buf the <tt>byte</tt> array that holds the packet.
-         * @param off the offset in <tt>buffer</tt> at which the actual data begins.
-         * @param len the number of <tt>byte</tt>s in <tt>buffer</tt> which
-         * constitute the actual data.
-         * @return <tt>true</tt> to allow the specified packet/<tt>buffer</tt> to be
-         * written into the arbitrary target/receiver that owns this
-         * {@link BitstreamController} ; otherwise, <tt>false</tt>
-         * @deprecated use accept(FrameDesc, RawPacket)
-         */
-        @Deprecated
-        public boolean accept(
-            FrameDesc sourceFrameDesc, byte[] buf, int off, int len)
-        {
-            return accept(sourceFrameDesc, new RawPacket(buf, off, len));
-        }
-
-        /**
          * Calculates the destination picture ID of an incoming VP8 frame.
          *
          * @param pkt the packet from which to calculate the vp8 picture id
@@ -1259,56 +1236,6 @@ public class SimulcastController
                     = start != -1 ? source.getStart() : source.getMinSeen();
 
                 int seqNum = pkt.getSequenceNumber();
-
-                boolean accept
-                    = RTPUtils.sequenceNumberDiff(seqNum, srcSeqNumLimit) <= 0;
-
-                if (!accept && logger.isDebugEnabled())
-                {
-                    logger.debug("frame_corruption seq=" + seqNum
-                        + ",seq_start=" + srcSeqNumStart
-                        + ",seq_limit=" + srcSeqNumLimit);
-                }
-
-                return accept;
-            }
-
-            /**
-             * Defines an RTP packet filter that controls which packets to be
-             * written into some arbitrary target/receiver that owns this
-             * {@link SeenFrame}.
-             *
-             * @param source the {@link FrameDesc} that the RTP packet belongs
-             * to.
-             * @param buf the <tt>byte</tt> array that holds the packet.
-             * @param off the offset in <tt>buffer</tt> at which the actual data
-             * begins.
-             * @param len the number of <tt>byte</tt>s in <tt>buffer</tt> which
-             * constitute the actual data.
-             * @return <tt>true</tt> to allow the specified packet/<tt>buffer</tt>
-             * to be written into the arbitrary target/receiver that owns this
-             * {@link SeenFrame} ; otherwise, <tt>false</tt>
-             */
-            boolean accept(FrameDesc source, byte[] buf, int off, int len)
-            {
-                if (this == mostRecentSentFrame /* the max frame can expand */
-                    || availableQualityIndices == null || availableQualityIndices.length < 2)
-                {
-                    int end = source.getEnd();
-                    srcSeqNumLimit
-                        = end != -1 ? source.getEnd() : source.getMaxSeen() ;
-                }
-
-                if (srcSeqNumLimit == -1)
-                {
-                    return false;
-                }
-
-                int start = source.getStart();
-                srcSeqNumStart
-                    = start != -1 ? source.getStart() : source.getMinSeen();
-
-                int seqNum = RawPacket.getSequenceNumber(buf, off, len);
 
                 boolean accept
                     = RTPUtils.sequenceNumberDiff(seqNum, srcSeqNumLimit) <= 0;
