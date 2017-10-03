ae8ee9b4e5ab64dba010ca28fb3cc2b12505520d
Дамян Минков <damencho@jitsi.org>
2017-10-03 11:29:37 -0500

    Moves callstats logic to jitsi-stats module. (#542)
    * Moves callstats logic to jitsi-stats module.
    
    * Updates jitsi-stats.
    
    Updates callstats to 4.1.4 and fixes some NPE and remote/local userIDs.

--- src/main/java/org/jitsi/videobridge/osgi/JvbBundleConfig.java
+++ src/main/java/org/jitsi/videobridge/osgi/JvbBundleConfig.java
@@ -15,7 +15,6 @@
  */
 package org.jitsi.videobridge.osgi;
 
-import java.io.*;
 import java.util.*;
 import org.ice4j.*;
 import org.ice4j.ice.harvest.*;
@@ -25,10 +24,9 @@ import org.jitsi.impl.neomedia.rtp.sendsidebandwidthestimation.*;
 import org.jitsi.impl.neomedia.transform.csrc.*;
 import org.jitsi.impl.neomedia.transform.srtp.*;
 import org.jitsi.meet.*;
-import org.jitsi.service.configuration.*;
 import org.jitsi.service.neomedia.*;
 import org.jitsi.service.packetlogging.*;
-import org.jitsi.util.*;
+import org.jitsi.stats.media.*;
 import org.jitsi.videobridge.cc.*;
 import org.jitsi.videobridge.xmpp.*;
 
@@ -113,92 +111,6 @@ public class JvbBundleConfig
     protected String[][] getBundlesImpl()
     {
         return BUNDLES;
-    }
-
-    /**
-     * Sets the default {@code System} properties on which the
-     * callstats-java-sdk library depends.
-     *
-     * @param defaults the {@code Map} in which the default {@code System}
-     * properties on which the callstats-java-sdk library depends are to be
-     * defined
-     */
-    private void getCallStatsJavaSDKSystemPropertyDefaults(
-            Map<String, String> defaults)
-    {
-        getCallStatsJavaSDKSystemPropertyDefaults(
-                "log4j2.xml",
-                defaults,
-                "log4j.configurationFile");
-        getCallStatsJavaSDKSystemPropertyDefaults(
-                "callstats-java-sdk.properties",
-                defaults,
-                "callstats.configurationFile");
-    }
-
-    /**
-     * Sets the default {@code System} properties on which the
-     * callstats-java-sdk library depends.
-     *
-     * @param fileName
-     * @param defaults the {@code Map} in which the default {@code System}
-     * properties on which the callstats-java-sdk library depends are to be
-     * defined
-     * @param propertyName
-     */
-    private void getCallStatsJavaSDKSystemPropertyDefaults(
-            String fileName,
-            Map<String, String> defaults,
-            String propertyName)
-    {
-        // There are multiple locations in which we may have put the log4j2.xml
-        // file. The callstats-java-sdk library defaults to config/log4j2.xml in
-        // the current directory. And that is where we keep the file in our
-        // source tree so that works when running from source. Unfortunately,
-        // such a location may not work for us when we run from the .deb
-        // package.
-
-        List<File> files = new ArrayList<>();
-
-        // Look for log4j2.xml in known locations under the current working
-        // directory.
-        files.add(new File("config", fileName));
-        files.add(new File(fileName));
-
-        // Additionally, look for log4j2.xml in the same known locations under
-        // SC_HOME_DIR_LOCATION/SC_HOME_DIR_NAME because that is a directory
-        // known to Jitsi-derived projects.
-        String scHomeDirName
-            = System.getProperty(
-                    ConfigurationService.PNAME_SC_HOME_DIR_NAME);
-
-        if (!StringUtils.isNullOrEmpty(scHomeDirName))
-        {
-            String scHomeDirLocation
-                = System.getProperty(
-                        ConfigurationService.PNAME_SC_HOME_DIR_LOCATION);
-
-            if (!StringUtils.isNullOrEmpty(scHomeDirLocation))
-            {
-                File dir = new File(scHomeDirLocation, scHomeDirName);
-
-                if (dir.isDirectory())
-                {
-                    for (int i = 0, end = files.size(); i < end; ++i)
-                        files.add(new File(dir, files.get(i).getPath()));
-                }
-            }
-        }
-
-        // Pick the first existing log4j2.xml from the candidates defined above.
-        for (File file : files)
-        {
-            if (file.exists())
-            {
-                defaults.put(propertyName, file.getAbsolutePath());
-                break;
-            }
-        }
     }
 
     @Override
@@ -292,7 +204,7 @@ public class JvbBundleConfig
         //        true_);
 
         // callstats-java-sdk
-        getCallStatsJavaSDKSystemPropertyDefaults(defaults);
+        Utils.getCallStatsJavaSDKSystemPropertyDefaults(defaults);
 
         return defaults;
     }
