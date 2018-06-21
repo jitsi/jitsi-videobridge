ae78367b639101b718fbcb53f66325e452df2016
brian baldino <bbaldino@gmail.com>
2018-06-21 15:43:23 -0700

    add BitBuffer tests

--- src/main/kotlin/org/jitsi/rtp/BitBuffer.kt
+++ src/main/kotlin/org/jitsi/rtp/BitBuffer.kt
@@ -3,65 +3,89 @@ package org.jitsi.rtp
 import com.sun.javaws.exceptions.InvalidArgumentException
 import java.nio.ByteBuffer
 
+/**
+ * Wraps a [ByteBuffer] and allows reading at a bit level
+ * (instead of a byte level).  This means that
+ * reads of bits will be done relative to the previous bit
+ * position in the current byte.  Only when the last bit of
+ * a byte is read will the buffer's position increment to
+ * the next byte.  Bit reads across byte boundaries is
+ * not supported.
+ *
+ * All reads of the [ByteBuffer] do not need to go through
+ * this class; changes in the [ByteBuffer]'s will cause
+ * [BitBuffer]'s bit offset to reset the next time a read is
+ * done.
+ */
 class BitBuffer(private val buf: ByteBuffer) {
     /**
      * The bit offset into the current byte
      */
     private var currBitPos: Int = 0
+
+    /**
+     * The byte we're reading from in the [ByteBuffer]
+     */
     private var currBytePos: Int = buf.position()
+
+    /**
+     * If the buffer's current position doesn't match where
+     * it was the last time we read from it, we'll assume we
+     * should reset to reading the first bit in the current
+     * byte rather than continuing to use any existing offset.
+     */
     private fun maybeResetCurrBitPos() {
-        // If the buffer isn't on the same byte as it was
-        // last time we read, then reset the current bit
-        // position
         if (buf.position() != currBytePos) {
             currBitPos = 0
             currBytePos = buf.position()
         }
     }
+
+    /**
+     * Make sure the amount of desired bits does not
+     * cross a byte boundary
+     */
     private fun validateOffset(numDesiredBits: Int) {
         if (currBitPos + numDesiredBits > 8) {
             throw InvalidArgumentException(arrayOf("$currBitPos", "$numDesiredBits"))
         }
     }
 
-    private fun maybeResetBufPosition(previousBufPosition: Int) {
+    /**
+     * If the most recent bit read did not reach a byte boundary,
+     * reset the buffer's position back to the current byte
+     * (since we're not done reading its bits yet)
+     */
+    private fun maybeResetBufPosition() {
         if (currBitPos < 8) {
-//            println("CurrBitPos is less than 8, reset to previous byte")
-            buf.position(previousBufPosition)
+            buf.rewindOneByte()
         }
     }
 
-    private fun moveCurrBitPos(numBitsRead: Int) {
-        // Subtract 1 since numBitsRead included a bit
-        // at the current position
-        currBitPos += numBitsRead
-//        println("Read $numBitsRead bits, curr bit position is now $currBitPos")
-    }
-
+    /**
+     * Get [numBits] bits starting at the current bit offset
+     * of the current byte. Returns them in the form of a [Byte]
+     */
     fun getBits(numBits: Int): Byte {
-//        println("Reading $numBits bits, current position is $currBitPos")
         maybeResetCurrBitPos()
         validateOffset(numBits)
-        val previousBufPosition = buf.position()
         val byte = buf.get()
         val result = byte.getBits(currBitPos, numBits)
-        moveCurrBitPos(numBits)
-        maybeResetBufPosition(previousBufPosition)
+        currBitPos += numBits
+        maybeResetBufPosition()
         return result
     }
 
+    /**
+     * Get the next bit interpreted as a [Boolean]
+     */
     fun getBitAsBoolean(): Boolean {
         maybeResetCurrBitPos()
         validateOffset(1)
-        val previousBufPosition = buf.position()
         val byte = buf.get()
         val result = byte.getBitAsBool(currBitPos)
-        moveCurrBitPos(1)
-        maybeResetBufPosition(previousBufPosition)
+        currBitPos += 1
+        maybeResetBufPosition()
         return result
     }
-
-    fun get(): Byte = buf.get()
-    fun getShort(): Short = buf.short
-    fun getInt(): Int = buf.int
 }
---
+++ src/test/java/org/jitsi/rtp/BitBufferTest.kt
@@ -0,0 +1,54 @@
+package org.jitsi.rtp
+
+import com.sun.javaws.exceptions.InvalidArgumentException
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.ShouldSpec
+import java.nio.ByteBuffer
+
+internal class BitBufferTest : ShouldSpec() {
+    override fun isInstancePerTest(): Boolean = true
+
+    private fun createBitBufferWithData(vararg bytes: Byte): BitBuffer {
+        return BitBuffer(ByteBuffer.wrap(bytes))
+    }
+
+    init {
+        "getBits" {
+            val buffer = ByteBuffer.wrap(byteArrayOf(
+                0xFF.toByte(),  0x01,           0x02,           0x03,
+                0x04,           0x05,           0x06,           0x06
+            ))
+            val bitBuffer = BitBuffer(buffer)
+            should("not advance to the next byte until its time") {
+                repeat(8) {
+                    bitBuffer.getBits(1).toInt() shouldBe 1
+                }
+                bitBuffer.getBits(1).toInt() shouldBe 0
+            }
+            should("throw when trying to cross a byte boundary") {
+                shouldThrow<InvalidArgumentException> {
+                    bitBuffer.getBits(10)
+                }
+            }
+        }
+        "getBitAsBoolean" {
+            should("correctly interpret a bit as boolean") {
+                createBitBufferWithData(0b10000000.toByte()).getBitAsBoolean() shouldBe true
+                createBitBufferWithData(0b00000000.toByte()).getBitAsBoolean() shouldBe false
+            }
+            "boundary cases" {
+                val buf = createBitBufferWithData(0x01, 0x00)
+                should("work fine on the last bit of a byte") {
+                    buf.getBits(7)
+                    buf.getBitAsBoolean() shouldBe true
+                }
+                should("work fine on the first bit of a new byte") {
+                    buf.getBits(7)
+                    buf.getBitAsBoolean()
+                    buf.getBitAsBoolean() shouldBe false
+                }
+            }
+        }
+    }
+}
