9a09a6f8c5b3f23c23e06e4786854016d920ae84
George Politis <gp@jitsi.org>
2018-08-03 14:45:51 -0500

    Minor improvements in the link-conditioner script.

--- resources/link-conditioner.sh
+++ resources/link-conditioner.sh
@@ -3,33 +3,35 @@
 PLR=${PLR:-'0.1'}
 DELAY=${DELAY:-100}
 BW=${BW:-'10Mbit/s'}
-PORT=10000
+PORT=${PORT:-10000}
+EGRESS_PIPE=1
+INGRESS_PIPE=2
 
 condition_egress() {
-   # Create an anchor and reload pf conf.
-   (cat /etc/pf.conf && echo "dummynet-anchor \"jitsi-videobridge\"" && echo "anchor \"jitsi-videobridge\"") | sudo pfctl -f -
+  # Create an anchor and reload pf conf.
+  (cat /etc/pf.conf && echo "dummynet-anchor \"jitsi-videobridge\"" && echo "anchor \"jitsi-videobridge\"") | sudo pfctl -f -
 
-   # Pipe all UDP traffic from port PORT through the egress pipe.
-   echo "dummynet out quick proto udp from any port $PORT to any pipe 1" | sudo pfctl -a jitsi-videobridge -f -
+  # Pipe all UDP traffic from port PORT through the EGRESS_PIPE.
+  echo "dummynet out quick proto udp from any port $PORT to any pipe $EGRESS_PIPE" | sudo pfctl -a jitsi-videobridge -f -
 
-   # Configure the egress pipe.
-   sudo dnctl pipe 1 config bw $BW plr $PLR delay $DELAY
+  # Configure the egress pipe.
+  sudo dnctl pipe $EGRESS_PIPE config bw $BW plr $PLR delay $DELAY
 }
 
 condition_ingress() {
-   # Create an anchor and reload pf conf.
-   (cat /etc/pf.conf && echo "dummynet-anchor \"jitsi-videobridge\"" && echo "anchor \"jitsi-videobridge\"") | sudo pfctl -f -
+  # Create an anchor and reload pf conf.
+  (cat /etc/pf.conf && echo "dummynet-anchor \"jitsi-videobridge\"" && echo "anchor \"jitsi-videobridge\"") | sudo pfctl -f -
 
-   # Pipe all UDP traffic to port PORT through the ingress pipe.
-   echo "dummynet out quick proto udp from any to any port $PORT pipe 2" | sudo pfctl -a jitsi-videobridge -f -
+  # Pipe all UDP traffic to port PORT through the INGRESS_PIPE.
+  echo "dummynet out quick proto udp from any to any port $PORT pipe $INGRESS_PIPE" | sudo pfctl -a jitsi-videobridge -f -
 
-   # Configure the ingress pipe.
-   sudo dnctl pipe 2 config bw $BW plr $PLR delay $DELAY
+  # Configure the ingress pipe.
+  sudo dnctl pipe $INGRESS_PIPE config bw $BW plr $PLR delay $DELAY
 }
 
 condition_flush() {
-   sudo dnctl -f flush
-   sudo pfctl -f /etc/pf.conf
+  sudo dnctl -f flush
+  sudo pfctl -f /etc/pf.conf
 }
 
 main() {
