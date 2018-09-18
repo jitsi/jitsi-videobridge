89703e093c4976ac403b6de40f22fc22db239037
brian baldino <bbaldino@gmail.com>
2018-09-18 13:02:47 -0700

    cleanup tcc generator

--- src/main/kotlin/org/jitsi/nlj/transform/node/incoming/TccGeneratorNode.kt
+++ src/main/kotlin/org/jitsi/nlj/transform/node/incoming/TccGeneratorNode.kt
@@ -17,11 +17,14 @@ package org.jitsi.nlj.transform.node.incoming
 
 import org.jitsi.nlj.Event
 import org.jitsi.nlj.PacketInfo
+import org.jitsi.nlj.ReceiveSsrcAddedEvent
+import org.jitsi.nlj.ReceiveSsrcRemovedEvent
 import org.jitsi.nlj.RtpExtensionAddedEvent
 import org.jitsi.nlj.RtpExtensionClearEvent
 import org.jitsi.nlj.forEachAs
 import org.jitsi.nlj.transform.node.Node
 import org.jitsi.nlj.util.appendLnIndent
+import org.jitsi.nlj.util.cdebug
 import org.jitsi.nlj.util.cinfo
 import org.jitsi.rtp.SrtpPacket
 import org.jitsi.rtp.rtcp.RtcpPacket
@@ -30,26 +33,32 @@ import org.jitsi.rtp.rtcp.rtcpfb.Tcc
 import org.jitsi.service.neomedia.RTPExtension
 import unsigned.toUInt
 
+/**
+ * Extract the TCC sequence numbers from each passing packet and generate
+ * a TCC packet to send transmit to the sender.
+ */
 class TccGeneratorNode(
     private val onTccPacketReady: (RtcpPacket) -> Unit = {}
 ) : Node("TCC generator") {
     private var tccExtensionId: Int? = null
     private var currTccSeqNum: Int = 0
     private var currTcc: Tcc = Tcc(feedbackPacketCount = currTccSeqNum++)
-    private var tempDetectedSsrc: Long? = null
+    /**
+     * Ssrc's we've been told this endpoint will transmit on.  We'll use an
+     * ssrc from this list for the RTCPFB mediaSourceSsrc field in the
+     * TCC packets we generate
+     */
+    private var mediaSsrcs: MutableSet<Long> = mutableSetOf()
     private var numTccSent: Int = 0
 
     override fun doProcessPackets(p: List<PacketInfo>) {
         val now = System.currentTimeMillis()
         tccExtensionId?.let { tccExtId ->
-            p.forEachAs<SrtpPacket> { packetInfo, pkt ->
+            p.forEachAs<SrtpPacket> { _, pkt ->
                 pkt.header.getExtension(tccExtId).let currPkt@ { tccExt ->
                     //TODO: check if it's a one byte or two byte ext?
                     val tccSeqNum = tccExt?.data?.getShort(0)?.toUInt() ?: return@currPkt
                     addPacket(tccSeqNum, now)
-                }
-                if (tempDetectedSsrc == null) {
-                    tempDetectedSsrc = pkt.header.ssrc
                 }
             }
         }
@@ -65,6 +74,8 @@ class TccGeneratorNode(
                 }
             }
             is RtpExtensionClearEvent -> tccExtensionId = null
+            is ReceiveSsrcAddedEvent -> mediaSsrcs.add(event.ssrc)
+            is ReceiveSsrcRemovedEvent -> mediaSsrcs.remove(event.ssrc)
         }
     }
 
@@ -72,12 +83,14 @@ class TccGeneratorNode(
         currTcc.addPacket(tccSeqNum, timestamp)
 
         if (isTccReadyToSend()) {
+            val mediaSsrc = if (mediaSsrcs.isNotEmpty()) mediaSsrcs.iterator().next() else -1L
             val pkt = RtcpFbTccPacket(
-                mediaSourceSsrc = tempDetectedSsrc!!,
+                mediaSourceSsrc = mediaSsrc,
                 referenceTime = currTcc.referenceTime,
                 feedbackPacketCount = currTcc.feedbackPacketCount,
                 packetInfo = currTcc.packetInfo
             )
+            logger.cdebug { "Sending TCC packet $pkt" }
             onTccPacketReady(pkt)
             numTccSent++
             // Create a new TCC instance for the next set of information
