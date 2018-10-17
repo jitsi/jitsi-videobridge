05c69de1d92b5065d39a5ca3acffbec476fc75f9
brian baldino <bbaldino@gmail.com>
2018-10-17 11:20:54 -0700

    add an rtp sender harness, clean up harness-related files

--- jitsi-media-transform/src/test/kotlin/org/jitsi/nlj/module_tests/PacketProducer.kt
+++ jitsi-media-transform/src/test/kotlin/org/jitsi/nlj/module_tests/PacketProducer.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jitsi.nlj
+package org.jitsi.nlj.module_tests
 
 typealias PacketReceiver = (org.jitsi.rtp.Packet) -> Unit
 
