2f1183f794ab52c8fcdaaae8847d1a8aad8c474b
Brian Baldino <bbaldino@gmail.com>
2019-01-10 10:25:59 -0800

    adapt to nlj node stat changes

--- src/main/java/org/jitsi/videobridge/Endpoint.java
+++ src/main/java/org/jitsi/videobridge/Endpoint.java
@@ -271,7 +271,7 @@ public class Endpoint
         }
         sctpManager.closeConnection();
 
-        logger.info(transceiver.getStats().prettyPrint(0));
+        logger.info(transceiver.getNodeStats().prettyPrint(0));
     }
 
     private DataChannelStack dataChannelStack;
