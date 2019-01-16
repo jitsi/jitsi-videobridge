5719fc19cd6504386b040257c44814eb24333bcb
Brian Baldino <bbaldino@gmail.com>
2019-01-16 11:03:34 -0800

    adapt to libjitsi updates (removal of FrameDesc)

--- src/main/kotlin/org/jitsi/nlj/rtp/VideoRtpPacket.kt
+++ src/main/kotlin/org/jitsi/nlj/rtp/VideoRtpPacket.kt
@@ -15,7 +15,6 @@
  */
 package org.jitsi.nlj.rtp
 
-import org.jitsi_modified.impl.neomedia.rtp.FrameDesc
 import org.jitsi.impl.neomedia.rtp.RTPEncodingDesc
 import org.jitsi.rtp.Packet
 import org.jitsi.rtp.RtpHeader
@@ -25,7 +24,6 @@ import java.nio.ByteBuffer
 
 class VideoRtpPacket : RtpPacket {
     var isKeyFrame: Boolean = false
-    var frameDesc: FrameDesc? = null
     var trackEncodings: Array<RTPEncodingDesc>? = null
 //    var rtpEncodingDesc: RTPEncodingDesc? = null
 
@@ -39,7 +37,6 @@ class VideoRtpPacket : RtpPacket {
     override fun clone(): Packet {
         val clone = VideoRtpPacket(getBuffer().clone())
         clone.isKeyFrame = isKeyFrame
-        clone.frameDesc = frameDesc
         clone.trackEncodings = trackEncodings
 
         return clone
