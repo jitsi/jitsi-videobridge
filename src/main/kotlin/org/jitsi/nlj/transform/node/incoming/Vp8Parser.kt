27787258d055b3332b0bad1711ed52bccf07c0ac
Brian Baldino <bbaldino@gmail.com>
2019-01-16 16:57:46 -0800

    add Vp8Parser to further parse incoming vp8 packets
    some fields (like spatial index) can't be determined from every packet.
    vp8parser has a memory so it can fill out fields in packets even if that
    packet doesn't have the necessary information to determine it.

---
+++ src/main/kotlin/org/jitsi/nlj/transform/node/incoming/Vp8Parser.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.nlj.transform.node.incoming
+
+import org.jitsi.nlj.PacketInfo
+import org.jitsi.nlj.codec.vp8.Vp8Utils
+import org.jitsi.nlj.forEachAs
+import org.jitsi.nlj.rtp.VideoRtpPacket
+import org.jitsi.nlj.rtp.codec.vp8.Vp8Packet
+import org.jitsi.nlj.transform.node.Node
+
+/**
+ * Some [Vp8Packet] fields are not able to be determined by looking at a single VP8 packet (for example the spatial
+ * layer index can only be acquired from keyframes).  This class keeps a longer-running 'memory' of the information
+ * needed to fill out fields like that in [Vp8Packet]s
+ */
+class Vp8Parser : Node("Vp8 parser") {
+    private val ssrcToSpatialLayerQuality: MutableMap<Long, Int> = HashMap()
+
+    override fun doProcessPackets(p: List<PacketInfo>) {
+        //TODO(brian): if we only route vp8 packets here, we can change this to expect Vp8Packet instead
+        p.forEachAs<VideoRtpPacket> { _, pkt ->
+            if (pkt is Vp8Packet) {
+                // If this was part of a keyframe, it will have already had it set
+                if (pkt.spatialLayerIndex > -1) {
+                    ssrcToSpatialLayerQuality.putIfAbsent(pkt.header.ssrc, pkt.spatialLayerIndex)
+                } else {
+                    pkt.spatialLayerIndex = ssrcToSpatialLayerQuality[pkt.header.ssrc] ?: -1
+                }
+                println("TEMP: fully parsed vp8 packet is now:\n $pkt")
+            }
+        }
+        next(p)
+    }
+}
