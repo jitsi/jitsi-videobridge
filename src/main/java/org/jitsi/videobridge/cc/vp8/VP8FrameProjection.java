131be79776ebcfabdffb1d4116d375a1ee0d132c
George Politis <gp@jitsi.org>
2019-02-01 11:16:10 +0000

    fix: Prevents NPE.

--- src/main/java/org/jitsi/videobridge/cc/vp8/VP8FrameProjection.java
+++ src/main/java/org/jitsi/videobridge/cc/vp8/VP8FrameProjection.java
@@ -353,7 +353,7 @@ public class VP8FrameProjection
             // packet of a frame, the first packet of another frame may have
             // already been accepted, which means there's no longer space to
             // piggyback anything.
-            if (accept(lastPacket))
+            if (lastPacket != null && accept(lastPacket))
             {
                 piggyBackedPackets.add(lastPacket);
             }
