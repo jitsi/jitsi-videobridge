1b264edb56e2394df8ab5f1b81e11ae664fdc229
Brian Baldino <bbaldino@gmail.com>
2019-02-08 11:28:12 -0800

    add timeexpiringcache helper

---
+++ src/main/kotlin/org/jitsi/nlj/util/TimeExpiringCache.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.nlj.util
+
+import java.time.Duration
+import java.util.TreeMap
+
+/**
+ * A cache which holds an arbitrary [DataType] and stores it, as well
+ * as the time at which it was added to the cache.  On updates, it will
+ * check to prune itself to remove anything older than the configured
+ * timeout.
+ * NOTE: it's possible the pruning does not remove ALL elements older
+ * than the configured timeout as we store the elements according to
+ * the value of their [IndexType], not their insertion timestamp.
+ * This is because retrieval is much handier when done via the value
+ * of [IndexType].  This means that it's possible older values will
+ * not be pruned when expected, but if the ordering of [IndexType]
+ * roughly follows the insertion order, it should not be too bad.
+ */
+//TODO(brian): add max size as well
+class TimeExpiringCache<IndexType, DataType>(
+    private val dataTimeout: Duration,
+    private val timeProvider: TimeProvider = SystemTimeProvider()
+) {
+    private val cache: TreeMap<IndexType, Container<DataType>> = TreeMap()
+
+    fun insert(index: IndexType, data: DataType) {
+        val container = Container(data, timeProvider.currentTimeMillis())
+        synchronized (cache) {
+            cache[index] = container
+            clean(timeProvider.currentTimeMillis() - dataTimeout.toMillis())
+        }
+    }
+
+    fun get(index: IndexType): DataType? {
+        synchronized (cache) {
+            val container = cache.getOrDefault(index, null) ?: return null
+            return container.data
+        }
+    }
+
+    private fun clean(expirationTimestamp: Long) {
+        synchronized (cache) {
+            val iter = cache.entries.iterator()
+            while (iter.hasNext()) {
+                val (_, container) = iter.next()
+                if (container.timeAdded <= expirationTimestamp) {
+                    iter.remove()
+                } else {
+                    // We'll break out of the loop once we find an insertion time that
+                    // is not older than the expiration cutoff
+                    break
+                }
+            }
+        }
+    }
+
+    /**
+     * Holds an instance of [Type] as well as when it was
+     * added to the data structure so that we can time out
+     * old [Container]s
+     */
+    private data class Container<Type>(
+            var data: Type,
+            var timeAdded: Long
+    )
+}
