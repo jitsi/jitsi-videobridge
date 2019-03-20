e8072fe625fa694c98bc3b7cc40bbc659bc4e576
Brian Baldino <bbaldino@gmail.com>
2019-03-20 11:07:35 -0700

    add static methods to TccHeaderExtension to interpret values from a NewRawPacket.HeaderExtension

--- src/main/kotlin/org/jitsi/rtp/rtp/header_extensions/TccHeaderExtension.kt
+++ src/main/kotlin/org/jitsi/rtp/rtp/header_extensions/TccHeaderExtension.kt
@@ -15,7 +15,10 @@
  */
 package org.jitsi.rtp.rtp.header_extensions
 
+import org.jitsi.rtp.NewRawPacket
+import org.jitsi.rtp.extensions.bytearray.putShort
 import org.jitsi.rtp.extensions.unsigned.toPositiveInt
+import org.jitsi.rtp.util.getShortAsInt
 import java.nio.ByteBuffer
 
 /**
@@ -40,6 +43,18 @@ class TccHeaderExtension(
         fun fromUnparsed(unparsedHeaderExtension: UnparsedHeaderExtension): TccHeaderExtension {
             val tccSeqNum = unparsedHeaderExtension.data.getShort().toPositiveInt()
             return TccHeaderExtension(unparsedHeaderExtension.id, tccSeqNum)
+        }
+
+        fun getSequenceNumber(ext: NewRawPacket.HeaderExtension): Int =
+            getSequenceNumber(ext.buffer, ext.offset, HeaderExtensionType.ONE_BYTE_HEADER_EXT)
+
+        /**
+         * [offset] into [buf] is the start of this entire extension (not the data section)
+         */
+        fun getSequenceNumber(buf: ByteArray, offset: Int, extType: HeaderExtensionType): Int =
+            buf.getShortAsInt(offset + extType.headerSizeBytes)
+        fun setSequenceNumber(buf: ByteArray, offset: Int, seqNum: Int, extType: HeaderExtensionType) {
+            buf.putShort(offset + extType.headerSizeBytes, seqNum.toShort())
         }
     }
 }
