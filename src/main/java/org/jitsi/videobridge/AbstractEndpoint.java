97a159dd3d6327202c29b26bd4506650acb9eaca
Brian Baldino <bbaldino@gmail.com>
2019-03-26 13:50:45 -0700

    move PotentialPacketHandler interface down to Endpoint

--- src/main/java/org/jitsi/videobridge/AbstractEndpoint.java
+++ src/main/java/org/jitsi/videobridge/AbstractEndpoint.java
@@ -37,7 +37,7 @@ import java.util.*;
  * @author Brian Baldino
  */
 public abstract class AbstractEndpoint extends PropertyChangeNotifier
-    implements EncodingsManager.EncodingsUpdateListener, PropertyChangeListener, PotentialPacketHandler
+    implements EncodingsManager.EncodingsUpdateListener, PropertyChangeListener
 {
     public static final String ENDPOINT_CHANGED_PROPERTY_NAME =
             AbstractEndpoint.class.getName() + ".endpoint_changed";
@@ -162,8 +162,7 @@ public abstract class AbstractEndpoint extends PropertyChangeNotifier
      * @return {@code true} if this endpoints wants to receive the packet, and
      * {@code false} otherwise.
      */
-    @Override
-    public boolean wants(PacketInfo packetInfo, String sourceEndpointId)
+    protected boolean wants(PacketInfo packetInfo, String sourceEndpointId)
     {
         // We always want audio packets
         if (packetInfo.getPacket() instanceof AudioRtpPacket)
