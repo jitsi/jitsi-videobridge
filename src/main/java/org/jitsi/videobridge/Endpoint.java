473ebd69635fed8ab27e4184a5464170ee6856cc
Brian Baldino <bbaldino@gmail.com>
2019-03-26 14:34:07 -0700

    change bitratecontroller#transformRtp to take PacketInfo

--- src/main/java/org/jitsi/videobridge/Endpoint.java
+++ src/main/java/org/jitsi/videobridge/Endpoint.java
@@ -458,7 +458,7 @@ public class Endpoint
             // different order) so we can't just reassign a transformed packet back into its
             // proper packetinfo.  need to change those classes to work with the new packet
             // types
-            org.jitsi.service.neomedia.RawPacket[] res = bitrateController.transformRtp(RawPacketExtensionsKt.toLegacyRawPacket(packet));
+            org.jitsi.service.neomedia.RawPacket[] res = bitrateController.transformRtp(packetInfo);
             for (org.jitsi.service.neomedia.RawPacket legacyRawPacket : res)
             {
                 if (legacyRawPacket == null)
