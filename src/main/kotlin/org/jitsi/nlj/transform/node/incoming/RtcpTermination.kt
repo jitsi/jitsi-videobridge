1de50f6af321cd2ced1198615b136f3dc9a90413
George Politis <gp@jitsi.org>
2019-04-17 15:17:12 +0100

    do not terminate rtcp sr in the ingress

--- src/main/kotlin/org/jitsi/nlj/transform/node/incoming/RtcpTermination.kt
+++ src/main/kotlin/org/jitsi/nlj/transform/node/incoming/RtcpTermination.kt
@@ -21,11 +21,12 @@ import org.jitsi.nlj.stats.NodeStatsBlock
 import org.jitsi.nlj.transform.node.TransformerNode
 import org.jitsi.nlj.util.cinfo
 import org.jitsi.rtp.rtcp.CompoundRtcpPacket
-import org.jitsi.rtp.rtcp.RtcpByePacket
 import org.jitsi.rtp.rtcp.RtcpPacket
-import org.jitsi.rtp.rtcp.RtcpRrPacket
-import org.jitsi.rtp.rtcp.RtcpSdesPacket
 import org.jitsi.rtp.rtcp.RtcpSrPacket
+import org.jitsi.rtp.rtcp.RtcpSdesPacket
+import org.jitsi.rtp.rtcp.RtcpRrPacket
+import org.jitsi.rtp.rtcp.RtcpByePacket
+import org.jitsi.rtp.rtcp.SenderInfoParser
 import org.jitsi.rtp.rtcp.rtcpfb.payload_specific_fb.RtcpFbFirPacket
 import org.jitsi.rtp.rtcp.rtcpfb.payload_specific_fb.RtcpFbPliPacket
 import org.jitsi.rtp.rtcp.rtcpfb.transport_layer_fb.RtcpFbNackPacket
@@ -45,7 +46,7 @@ class RtcpTermination(
         compoundRtcp.packets.forEach { pkt ->
             when (pkt) {
                 is RtcpFbTccPacket -> handleTccPacket(pkt)
-                is RtcpFbPliPacket, is RtcpFbFirPacket -> {
+                is RtcpFbPliPacket, is RtcpFbFirPacket, is RtcpSrPacket -> {
                     // We'll let these pass through and be forwarded to the sender who will be
                     // responsible for translating/aggregating them
                     // NOTE(brian): this should work fine as long as we can't receive 2 RTCP packets
@@ -64,6 +65,12 @@ class RtcpTermination(
             // TODO: keep an eye on if anything in here takes a while it could slow the packet pipeline down
             packetReceiveCounts.merge(pkt::class.simpleName!!, 1, Int::plus)
             rtcpEventNotifier.notifyRtcpReceived(pkt, packetInfo.receivedTime)
+
+            if (pkt is RtcpSrPacket) {
+                // NOTE(george) effectively eliminates any report blocks as we don't want to relay those
+                logger.cinfo { "saw an sr from ssrc=${pkt.senderSsrc}, timestamp=${pkt.senderInfo.rtpTimestamp}"}
+                pkt.length = SenderInfoParser.SIZE_BYTES
+            }
         }
         return if (forwardedRtcp != null) {
             // Manually cast to RtcpPacket as a workaround for https://youtrack.jetbrains.com/issue/KT-7186
