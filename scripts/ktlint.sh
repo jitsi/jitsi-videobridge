716c06565199542fc27d1573152b7750dbeb8ed4
Boris Grozev <boris@jitsi.org>
2019-04-17 07:21:16 +0100

    ktlint: Do NOT use --apply-to-idea-project
    because it silently overrides the *Java* code style settings. Do not
    run in the mvn validate phase. Use pre-commit hook instead of pre-push.

--- scripts/ktlint.sh
+++ scripts/ktlint.sh
@@ -1,8 +1,5 @@
 # Install the ktlint command-line utility
 curl -sSLO https://github.com/shyiko/ktlint/releases/download/0.31.0/ktlint && chmod a+x ktlint && sudo mv ktlint /usr/local/bin/
 
-# Integrate ktlint with Intellij.  This makes Intellij's built-in formatter produce 100% ktlint-compatible ode
-ktlint --apply-to-idea-project
-
 # Install a pre-push hook to check for violations
-ktlint --install-git-pre-push-hook
+ktlint --install-git-pre-commit-hook
