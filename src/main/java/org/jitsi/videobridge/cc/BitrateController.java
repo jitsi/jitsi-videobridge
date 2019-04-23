cd6b219733a1b81da3be172093de9e2cc4124f9c
George Politis <gp@jitsi.org>
2019-04-23 13:56:29 +0100

    removes unused field

--- src/main/java/org/jitsi/videobridge/cc/BitrateController.java
+++ src/main/java/org/jitsi/videobridge/cc/BitrateController.java
@@ -1257,11 +1257,6 @@ public class BitrateController
         private final int targetSSRC;
 
         /**
-         * Maximum frame height, in pixels, for any video stream forwarded to this receiver
-         */
-        private final int maxFrameHeight;
-
-        /**
          * The first {@link MediaStreamTrackDesc} of the {@link Endpoint} that
          * this instance pertains to.
          */
@@ -1320,7 +1315,6 @@ public class BitrateController
             this.selected = selected;
             this.fitsInLastN = fitsInLastN;
             this.track = track;
-            this.maxFrameHeight = maxFrameHeight;
 
             RTPEncodingDesc[] encodings;
             if (track == null)
