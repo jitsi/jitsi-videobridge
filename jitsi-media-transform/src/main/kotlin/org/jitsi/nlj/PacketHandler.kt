5e6b22823422bd7ad2bebe2a07132d6b903100c7
Brian Baldino <bbaldino@gmail.com>
2019-05-30 10:50:31 -0700

    move packet handler stats to a separate class
    this change aims to address 2 main things:
    1) there were a bunch of 'loose' stats all around a central thing (the
    rate/amount of packets handled).  these were all held as separate
    variables directly in RtpReceiver and i wanted to group them
    2) in addition to being grouped, they are common to all packet handlers
    and may be useful in other places, so i moved them into a class which a
    packet handler can derive from to track those stats.
    
    also, these were somewhat intermixed with stats related to the queue
    (read/write rate, etc.).  those stats will be moved to the queue in an
    upcoming commit.

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/PacketHandler.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/PacketHandler.kt
@@ -15,9 +15,67 @@
  */
 package org.jitsi.nlj
 
+import org.jitsi.nlj.stats.NodeStatsBlock
+import org.jitsi.nlj.transform.NodeStatsProducer
+import org.jitsi.nlj.util.addMbps
+
 interface PacketHandler {
     /**
      * Process the given packets
      */
     fun processPacket(packetInfo: PacketInfo)
+}
+
+/**
+ * A [PacketHandler] which tracks statistics about the packets it handles
+ */
+abstract class StatsKeepingPacketHandler : PacketHandler, NodeStatsProducer {
+    private val statistics = Statistics()
+
+    final override fun processPacket(packetInfo: PacketInfo) {
+        val now = System.currentTimeMillis()
+        statistics.packetsReceived++
+        statistics.bytesReceived += packetInfo.packet.length
+        if (statistics.firstPacketReceivedTime == 0L) {
+            statistics.firstPacketReceivedTime = now
+        }
+        statistics.lastPacketReceivedTime = now
+
+        doProcessPacket(packetInfo)
+    }
+
+    /**
+     * We override [processPacket] above and prevent subclasses from doing so
+     * to make sure this class sees all packets that pass through it and it
+     * can track stats correctly.  This method should be implemented by
+     * subclasses as a stand-in for [processPacket], as it is called once
+     * all the stat-tracking has been done.
+     */
+    protected abstract fun doProcessPacket(packetInfo: PacketInfo)
+
+    override fun getNodeStats(): NodeStatsBlock = statistics.getNodeStats()
+
+    /**
+     * Statistics common to [PacketHandler]s which are interesting to track
+     */
+    data class Statistics(
+        var firstPacketReceivedTime: Long = 0,
+        var lastPacketReceivedTime: Long = 0,
+        var bytesReceived: Long = 0,
+        var packetsReceived: Long = 0
+    ) : NodeStatsProducer {
+        override fun getNodeStats(): NodeStatsBlock = NodeStatsBlock("Packet handler stats").apply {
+            addNumber(RECEIVED_PACKETS, packetsReceived)
+            addNumber(RECEIVED_BYTES, bytesReceived)
+            addNumber(RECEIVED_DURATION_MS, lastPacketReceivedTime - firstPacketReceivedTime)
+            addMbps(RECEIVED_BITRATE_MBPS, RECEIVED_BYTES, RECEIVED_DURATION_MS)
+        }
+
+        companion object {
+            private const val RECEIVED_PACKETS = "received_packets"
+            private const val RECEIVED_BYTES = "received_bytes"
+            private const val RECEIVED_DURATION_MS = "received_duration_ms"
+            private const val RECEIVED_BITRATE_MBPS = "received_bitrate_mbps"
+        }
+    }
 }
