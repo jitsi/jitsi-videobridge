ac232f581973525809780accc587f32e03694ad3
Brian Baldino <bbaldino@gmail.com>
2019-05-30 10:50:31 -0700

    move packet handler stats to a separate class
    this change aims to address 2 main things:
    1) there were a bunch of 'loose' stats all around a central thing (the
    rate/amount of packets handled).  these were all held as separate
    variables directly in RtpReceiver and i wanted to group them
    2) in addition to being grouped, they are common to all packet handlers
    and may be useful in other places, so i moved them into a class which a
    packet handler can derive from to track those stats.
    
    also, these were somewhat intermixed with stats related to the queue
    (read/write rate, etc.).  those stats will be moved to the queue in an
    upcoming commit.

--- src/main/kotlin/org/jitsi/nlj/RtpReceiverImpl.kt
+++ src/main/kotlin/org/jitsi/nlj/RtpReceiverImpl.kt
@@ -45,15 +45,12 @@ import org.jitsi.nlj.transform.packetPath
 import org.jitsi.nlj.transform.pipeline
 import org.jitsi.nlj.util.PacketInfoQueue
 import org.jitsi.nlj.util.PacketPredicate
-import org.jitsi.nlj.util.addMbps
-import org.jitsi.nlj.util.addRatio
 import org.jitsi.nlj.util.cdebug
 import org.jitsi.nlj.util.getLogger
 import org.jitsi.rtp.rtcp.RtcpPacket
 import org.jitsi.util.RTCPUtils
 import org.jitsi.utils.logging.Logger
 import org.jitsi_modified.impl.neomedia.rtp.TransportCCEngine
-import java.lang.Double.max
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.ScheduledExecutorService
 
@@ -97,17 +94,10 @@ class RtpReceiverImpl @JvmOverloads constructor(
         private val classLogger: Logger = Logger.getLogger(this::class.java)
         private const val PACKET_QUEUE_ENTRY_EVENT = "Entered RTP receiver incoming queue"
         private const val PACKET_QUEUE_EXIT_EVENT = "Exited RTP receiver incoming queue"
-
-        private const val RECEIVED_BYTES = "received_bytes"
-        private const val RECEIVED_DURATION_MS = "received_duration_ms"
-        private const val PROCESSED_BYTES = "processed_bytes"
-        private const val PROCESSING_DURATION_MS = "processing_duration_ms"
-        private const val QUEUE_NUM_READS = "queue_num_reads"
-        private const val QUEUE_READ_DURATION_S = "queue_read_duration_s"
     }
 
     /**
-     * [rtpPacketHandler] will be invoked with RTP packets that have made
+     * [packetHandler] will be invoked with RTP packets that have made
      * it through the entire receive pipeline.  Some external entity should
      * assign it to a [PacketHandler] with appropriate logic.
      */
@@ -125,21 +115,6 @@ class RtpReceiverImpl @JvmOverloads constructor(
             packetHandler?.processPacket(packetInfo) ?: packetDiscarded(packetInfo)
         }
     }
-
-    // Stat tracking values
-    var firstPacketWrittenTime: Long = 0
-    var lastPacketWrittenTime: Long = 0
-    var bytesReceived: Long = 0
-    var packetsReceived: Long = 0
-
-    var bytesProcessed: Long = 0
-    var packetsProcessed: Long = 0
-    var firstPacketProcessedTime: Long = 0
-    var lastPacketProcessedTime: Long = 0
-
-    private var firstQueueReadTime: Long = -1
-    private var lastQueueReadTime: Long = -1
-    private var numQueueReads: Long = 0
 
     init {
         logger.cdebug { "Receiver ${this.hashCode()} using executor ${executor.hashCode()}" }
@@ -205,57 +180,25 @@ class RtpReceiverImpl @JvmOverloads constructor(
 
     private fun handleIncomingPacket(packet: PacketInfo): Boolean {
         if (running) {
-            val now = System.currentTimeMillis()
-            if (firstQueueReadTime == -1L) {
-                firstQueueReadTime = now
-            }
-            numQueueReads++
-            lastQueueReadTime = now
             packet.addEvent(PACKET_QUEUE_EXIT_EVENT)
-            bytesProcessed += packet.packet.length
-            packetsProcessed++
-            if (firstPacketProcessedTime == 0L) {
-                firstPacketProcessedTime = System.currentTimeMillis()
-            }
-            lastPacketProcessedTime = System.currentTimeMillis()
             processPacket(packet)
             return true
         }
         return false
     }
 
-    override fun processPacket(packetInfo: PacketInfo) = inputTreeRoot.processPacket(packetInfo)
+    override fun doProcessPacket(packetInfo: PacketInfo) = inputTreeRoot.processPacket(packetInfo)
 
     override fun getNodeStats(): NodeStatsBlock = NodeStatsBlock("RTP receiver $id").apply {
-        addNumber("received_packets", packetsReceived)
-        addNumber(RECEIVED_BYTES, bytesReceived)
-        addNumber(RECEIVED_DURATION_MS, lastPacketWrittenTime - firstPacketProcessedTime)
-        addMbps("received_bitrate_mbps", RECEIVED_BYTES, RECEIVED_DURATION_MS)
-
-        addNumber("processed_packets", packetsProcessed)
-        addNumber("processed_packets_percent", 100 * (packetsProcessed / max(1.0, packetsReceived.toDouble())))
-        addNumber(PROCESSED_BYTES, bytesProcessed)
-        addNumber(PROCESSING_DURATION_MS, lastPacketProcessedTime - firstPacketProcessedTime)
-        addMbps("processed_bitrate", PROCESSED_BYTES, PROCESSING_DURATION_MS)
-
-        addNumber(QUEUE_NUM_READS, numQueueReads)
-        addNumber(QUEUE_READ_DURATION_S, (lastQueueReadTime - firstQueueReadTime).toDouble() / 1000)
-        addRatio("queue_average_reads_per_second", QUEUE_NUM_READS, QUEUE_READ_DURATION_S)
-
+        addBlock(super.getNodeStats())
         addString("running", running.toString())
         NodeStatsVisitor(this).visit(inputTreeRoot)
     }
 
     override fun enqueuePacket(p: PacketInfo) {
 //        logger.cinfo { "Receiver $id enqueing data" }
-        bytesReceived += p.packet.length
         p.addEvent(PACKET_QUEUE_ENTRY_EVENT)
         incomingPacketQueue.add(p)
-        packetsReceived++
-        if (firstPacketWrittenTime == 0L) {
-            firstPacketWrittenTime = System.currentTimeMillis()
-        }
-        lastPacketWrittenTime = System.currentTimeMillis()
     }
 
     override fun setSrtpTransformers(srtpTransformers: SrtpTransformers) {
