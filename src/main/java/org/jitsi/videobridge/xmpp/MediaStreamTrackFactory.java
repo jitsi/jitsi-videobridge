ef0ce9033c1827ebcad4a25d77ac133649091959
George Politis <gp@jitsi.org>
2019-05-31 14:54:37 +0100

    adapts to jmt changes

--- src/main/java/org/jitsi/videobridge/xmpp/MediaStreamTrackFactory.java
+++ src/main/java/org/jitsi/videobridge/xmpp/MediaStreamTrackFactory.java
@@ -733,6 +733,8 @@ public class MediaStreamTrackFactory
         assert(encodings.length <= numEncodings);
         System.arraycopy(encodings, 0, rtpEncodings, 0, encodings.length);
 
+        track.updateEncodingCache();
+
         return track;
     }
 }
