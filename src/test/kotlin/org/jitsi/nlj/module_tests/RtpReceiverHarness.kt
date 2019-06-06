a9f4c85b5765a9b917ce3bce617d1a9147e4eee3
Brian Baldino <bbaldino@gmail.com>
2019-06-06 10:12:57 -0700

    init libjitsi to get srtp working in receiver harness

--- src/test/kotlin/org/jitsi/nlj/module_tests/RtpReceiverHarness.kt
+++ src/test/kotlin/org/jitsi/nlj/module_tests/RtpReceiverHarness.kt
@@ -18,6 +18,7 @@ package org.jitsi.nlj.module_tests
 import org.jitsi.nlj.PacketInfo
 import org.jitsi.nlj.RtpReceiver
 import org.jitsi.nlj.util.safeShutdown
+import org.jitsi.service.libjitsi.LibJitsi
 import org.jitsi.test_utils.Pcaps
 import java.time.Duration
 import java.util.concurrent.Executors
@@ -35,6 +36,9 @@ import kotlin.system.measureTimeMillis
  */
 
 fun main(args: Array<String>) {
+    // We need to start libjitsi so that the openssl lib gets loaded.
+    LibJitsi.start()
+
     val pcap = Pcaps.Incoming.ONE_PARTICIPANT_RTP_RTCP_SIM_RTX
 
     val producer = PcapPacketProducer(pcap.filePath)
