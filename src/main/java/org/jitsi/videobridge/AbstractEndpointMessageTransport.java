ffac5f1b9bd0c30a060678072f65053b1c893266
bgrozev <boris@jitsi.org>
2019-07-18 11:02:34 +0100

    Fix license headers. (#859)

--- src/main/java/org/jitsi/videobridge/AbstractEndpointMessageTransport.java
+++ src/main/java/org/jitsi/videobridge/AbstractEndpointMessageTransport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright @ 2017-2018 Atlassian Pty Ltd
+ * Copyright @ 2017 - Present, 8x8 Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
