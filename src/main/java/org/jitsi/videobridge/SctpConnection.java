92931d9acb466b4f705a650dea6df36e1bc5db34
George Politis <gp@jitsi.org>
2019-07-24 12:23:24 +0200

    mvn: updates jitsi-utils@1.0-20190723.110846-12

--- src/main/java/org/jitsi/videobridge/SctpConnection.java
+++ src/main/java/org/jitsi/videobridge/SctpConnection.java
@@ -34,7 +34,7 @@ import org.jitsi.service.libjitsi.*;
 import org.jitsi.service.neomedia.*;
 import org.jitsi.service.packetlogging.*;
 import org.jitsi.util.*;
-import org.jitsi.utils.*;
+import org.jitsi.utils.queue.*;
 import org.jitsi.utils.concurrent.ExecutorUtils;
 import org.jitsi.utils.logging.Logger; // Disambiguation.
 
