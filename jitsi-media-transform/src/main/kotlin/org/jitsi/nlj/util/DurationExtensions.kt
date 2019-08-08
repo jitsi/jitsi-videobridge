b6b2e7a76483b1cb40f42a88a353268d5bdcfc45
brian baldino <bbaldino@gmail.com>
2019-08-08 12:35:59 -0700

    add tcc generator test

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/util/DurationExtensions.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/util/DurationExtensions.kt
@@ -3,3 +3,7 @@ package org.jitsi.nlj.util
 import java.time.Duration
 
 fun Int.milliseconds(): Duration = Duration.ofMillis(this.toLong())
+fun Int.ms(): Duration = Duration.ofMillis(this.toLong())
+
+fun Int.minutes(): Duration = Duration.ofMinutes(this.toLong())
+fun Int.mins(): Duration = Duration.ofMinutes(this.toLong())
