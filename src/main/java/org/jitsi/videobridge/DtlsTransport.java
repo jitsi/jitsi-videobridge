514a84e37de3f8266f50da38b8cbb7c315d3925a
Yury Yaroshevich <yura.yaroshevich@gmail.com>
2019-09-24 23:31:05 +0300

    Fill proper DTLS setup value when transport is described. (#924)
    * Fill proper DTLS setup value when described.
    
    * Fixed role description after review.
    
    * Added extra comment from feedback.

--- src/main/java/org/jitsi/videobridge/DtlsTransport.java
+++ src/main/java/org/jitsi/videobridge/DtlsTransport.java
@@ -246,8 +246,26 @@ public class DtlsTransport extends IceTransport
         fingerprintPE.setFingerprint(dtlsStack.getLocalFingerprint());
         fingerprintPE.setHash(dtlsStack.getLocalFingerprintHashFunction());
 
-        // TODO: don't we only support ACTIVE right now?
-        fingerprintPE.setSetup("ACTPASS");
+        final DtlsRole role = dtlsStack.getRole();
+        final String setupRole;
+        if (role == null)
+        {
+            // We've not chosen a role yet, so we can be either
+            setupRole = "actpass";
+        }
+        else if (role instanceof DtlsServer)
+        {
+            setupRole = "passive";
+        }
+        else if (role instanceof DtlsClient)
+        {
+            setupRole = "active";
+        }
+        else
+        {
+            throw new IllegalStateException("Can not describe role " + role);
+        }
+        fingerprintPE.setSetup(setupRole);
     }
 
     /**
