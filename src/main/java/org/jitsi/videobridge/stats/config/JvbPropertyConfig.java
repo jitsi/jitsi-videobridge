298287d3bc23fe6af7e2a1b6f87e73997210d574
Brian Baldino <bbaldino@gmail.com>
2019-10-08 12:33:03 -0700

    add jvb prop config wrapper to expose legacy/new config helpers

---
+++ src/main/java/org/jitsi/videobridge/stats/config/JvbPropertyConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.videobridge.stats.config;
+
+import com.typesafe.config.*;
+import org.jitsi.utils.config.*;
+import org.jitsi.videobridge.util.config.*;
+
+import java.util.function.*;
+
+public class JvbPropertyConfig<T> extends PropertyConfig<T>
+{
+    public JvbPropertyConfig<T> fromLegacyConfig(Function<Config, T> getter)
+    {
+        suppliedBy(new LegacyConfigValueSupplier<>(getter));
+
+        return this;
+    }
+
+    public JvbPropertyConfig<T> fromNewConfig(Function<Config, T> getter)
+    {
+        suppliedBy(new ConfigValueSupplier<T>(getter));
+
+        return this;
+    }
+}
