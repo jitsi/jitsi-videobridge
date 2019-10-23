181574d64cd00974a1fe4977be06083cd761b4ee
Jonathan Lennox <jonathan.lennox@8x8.com>
2019-10-23 12:46:19 -0400

    Use 'stubOnly = true' for Mockito in BandwidthEstimationTest. (#140)
    * Use 'stubOnly = true' for Mockito in BandwidthEstimationTest.
    
    Otherwise the test consumes a huge amount of memory (gigabytes) and takes a long time.
    
    * Create a stubOnlySpy() wrapper for Mockito.

---
+++ src/test/kotlin/org/jitsi/nlj/test_utils/MockitoUtils.kt
@@ -0,0 +1,12 @@
+package org.jitsi.nlj.test_utils
+
+import com.nhaarman.mockitokotlin2.UseConstructor
+import com.nhaarman.mockitokotlin2.mock
+import org.mockito.Mockito
+
+/**
+ * Like Mockito spy(), but with stubOnly set to avoid excessive memory consumption.
+ */
+inline fun <reified T : Any> stubOnlySpy(): T {
+    return mock(stubOnly = true, useConstructor = UseConstructor.parameterless(), defaultAnswer = Mockito.CALLS_REAL_METHODS)
+}
