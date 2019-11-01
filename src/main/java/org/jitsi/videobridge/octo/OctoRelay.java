209f226e8badf43d3dd9d79b848d65b1ff6a5a80
bbaldino <bbaldino@gmail.com>
2019-11-01 10:26:32 -0700

    Octo logging (#961)
    * use endpoint logger as parent for dtls transport
    
    this way we can get the ep id context
    
    * use new logger in octo relay
    
    * log expiration of an octo tentacle
    
    * log addition/removal of octo conf handlers

--- src/main/java/org/jitsi/videobridge/octo/OctoRelay.java
+++ src/main/java/org/jitsi/videobridge/octo/OctoRelay.java
@@ -19,7 +19,8 @@ import org.ice4j.socket.*;
 import org.jitsi.rtp.*;
 import org.jitsi.rtp.rtp.*;
 import org.jitsi.utils.*;
-import org.jitsi.utils.logging.*;
+import org.jitsi.utils.collections.*;
+import org.jitsi.utils.logging2.*;
 import org.jitsi.utils.stats.*;
 import org.jitsi.videobridge.util.*;
 import org.json.simple.*;
@@ -48,8 +49,7 @@ public class OctoRelay
      * The {@link Logger} used by the {@link OctoRelay} class and its
      * instances to print debug information.
      */
-    private static final Logger logger
-        = Logger.getLogger(OctoRelay.class);
+    private final Logger logger;
 
     /**
      * The receive buffer size to set of the socket.
@@ -130,6 +130,10 @@ public class OctoRelay
     OctoRelay(String address, int port)
         throws UnknownHostException, SocketException
     {
+        logger = new LoggerImpl(
+            OctoRelay.class.getName(),
+            new LogContext(JMap.of("address", address, "port", Integer.toString(port)))
+        );
         InetSocketAddress addr
                 = new InetSocketAddress(InetAddress.getByName(address), port);
         socket = new DatagramSocket(addr);
@@ -438,6 +442,7 @@ public class OctoRelay
     {
         synchronized (packetHandlers)
         {
+            logger.info("Adding handler for conference " + conferenceId);
             if (packetHandlers.containsKey(conferenceId))
             {
                 logger.warn("Replacing an existing packet handler for gid="
@@ -462,6 +467,7 @@ public class OctoRelay
             PacketHandler existingHandler = packetHandlers.get(conferenceId);
             if (handler == existingHandler)
             {
+                logger.info("Removing handler for conference " + conferenceId);
                 packetHandlers.remove(conferenceId);
             }
         }
