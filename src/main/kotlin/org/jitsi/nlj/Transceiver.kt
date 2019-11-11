8bacf7e5b9d0f64da976ba245449b3c439e21ab5
bbaldino <bbaldino@gmail.com>
2019-11-11 10:18:44 -0800

    Packet activity updates (#148)
    * change packet io activity to use instants
    
    * add clock member in transceiver
    
    * use new packet activity fields in transceiver
    
    * wire deprecated io activity props to new ones
    
    * only increase activity times
    
    * make packet times threadsafe; add tests
    
    * add docs

--- src/main/kotlin/org/jitsi/nlj/Transceiver.kt
+++ src/main/kotlin/org/jitsi/nlj/Transceiver.kt
@@ -66,7 +66,8 @@ class Transceiver(
      */
     backgroundExecutor: ScheduledExecutorService,
     diagnosticContext: DiagnosticContext,
-    parentLogger: Logger
+    parentLogger: Logger,
+    private val clock: Clock = Clock.systemUTC()
 ) : Stoppable, NodeStatsProducer {
     private val logger = parentLogger.createChildLogger(Transceiver::class)
     val packetIOActivity = PacketIOActivity()
@@ -127,7 +128,7 @@ class Transceiver(
      * this transceiver is associated with) to be processed by the receiver pipeline.
      */
     fun handleIncomingPacket(p: PacketInfo) {
-        packetIOActivity.lastPacketReceivedTimestampMs = System.currentTimeMillis()
+        packetIOActivity.lastRtpPacketReceivedTimestamp = clock.instant()
         rtpReceiver.enqueuePacket(p)
     }
 
@@ -136,7 +137,7 @@ class Transceiver(
      * passing them out the sender's outgoing pipeline
      */
     fun sendPacket(packetInfo: PacketInfo) {
-        packetIOActivity.lastPacketSentTimestampMs = System.currentTimeMillis()
+        packetIOActivity.lastRtpPacketSentTimestamp = clock.instant()
         rtpSender.processPacket(packetInfo)
     }
 
@@ -257,7 +258,7 @@ class Transceiver(
             addBlock(streamInformationStore.getNodeStats())
             addBlock(mediaStreamTracks.getNodeStats())
             addString("endpointConnectionStats", endpointConnectionStats.getSnapshot().toString())
-            addJson("Bandwidth Estimation", bandwidthEstimator.getStats(Clock.systemUTC().instant()).toJson())
+            addJson("Bandwidth Estimation", bandwidthEstimator.getStats(clock.instant()).toJson())
             addBlock(rtpReceiver.getNodeStats())
             addBlock(rtpSender.getNodeStats())
         }
@@ -273,7 +274,7 @@ class Transceiver(
             rtpReceiver.getPacketStreamStats(),
             rtpSender.getStreamStats(),
             rtpSender.getPacketStreamStats(),
-            bandwidthEstimator.getStats(Clock.systemUTC().instant()))
+            bandwidthEstimator.getStats(clock.instant()))
     }
 
     override fun stop() {
---
+++ src/test/kotlin/org/jitsi/nlj/stats/PacketIOActivityTest.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.nlj.stats
+
+import com.nhaarman.mockitokotlin2.spy
+import io.kotlintest.IsolationMode
+import io.kotlintest.minutes
+import io.kotlintest.seconds
+import io.kotlintest.specs.ShouldSpec
+import io.kotlintest.shouldBe
+import org.jitsi.nlj.test_utils.FakeClock
+
+class PacketIOActivityTest : ShouldSpec() {
+    override fun isolationMode(): IsolationMode? = IsolationMode.InstancePerLeaf
+
+    private val packetIoActivity = PacketIOActivity()
+    private val clock: FakeClock = spy()
+
+    init {
+        "Last packet time values" {
+            clock.elapse(1.minutes)
+            val oldTime = clock.instant()
+            clock.elapse(1.minutes)
+            val newTime = clock.instant()
+            packetIoActivity.lastRtpPacketSentTimestamp = newTime
+            packetIoActivity.lastRtpPacketReceivedTimestamp = newTime
+            packetIoActivity.lastIceActivityTimestamp = newTime
+            "when setting an older time" {
+                packetIoActivity.lastRtpPacketSentTimestamp = oldTime
+                packetIoActivity.lastRtpPacketReceivedTimestamp = oldTime
+                packetIoActivity.lastIceActivityTimestamp = oldTime
+                should("not allow going backwards") {
+                    packetIoActivity.lastRtpPacketSentTimestamp shouldBe newTime
+                    packetIoActivity.lastRtpPacketReceivedTimestamp shouldBe newTime
+                    packetIoActivity.lastIceActivityTimestamp shouldBe newTime
+                }
+            }
+        }
+        "lastOverallRtpActivity" {
+            should("only reflect RTP packet time values") {
+                clock.elapse(30.seconds)
+                val rtpSentTime = clock.instant()
+                clock.elapse(5.seconds)
+                val rtpReceivedTime = clock.instant()
+                clock.elapse(10.seconds)
+                val iceTime = clock.instant()
+                packetIoActivity.lastRtpPacketSentTimestamp = rtpSentTime
+                packetIoActivity.lastRtpPacketReceivedTimestamp = rtpReceivedTime
+                packetIoActivity.lastIceActivityTimestamp = iceTime
+                packetIoActivity.lastOverallRtpActivity shouldBe rtpReceivedTime
+            }
+        }
+        "lastOverallActivity" {
+            should("only reflect all packet time values") {
+                clock.elapse(30.seconds)
+                val rtpSentTime = clock.instant()
+                clock.elapse(10.seconds)
+                val iceTime = clock.instant()
+                clock.elapse(5.seconds)
+                val rtpReceivedTime = clock.instant()
+                packetIoActivity.lastRtpPacketSentTimestamp = rtpSentTime
+                packetIoActivity.lastRtpPacketReceivedTimestamp = rtpReceivedTime
+                packetIoActivity.lastIceActivityTimestamp = iceTime
+                packetIoActivity.lastOverallRtpActivity shouldBe rtpReceivedTime
+            }
+        }
+    }
+}
