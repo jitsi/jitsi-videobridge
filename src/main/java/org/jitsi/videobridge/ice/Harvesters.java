2039e6d249ff91a85526289ac457bdb6b3aa669c
Brian Baldino <bbaldino@gmail.com>
2020-01-15 10:06:21 -0800

    remove stale javadoc

--- src/main/java/org/jitsi/videobridge/ice/Harvesters.java
+++ src/main/java/org/jitsi/videobridge/ice/Harvesters.java
@@ -65,10 +65,6 @@ public class Harvesters
      * Initializes the static <tt>Harvester</tt> instances used by all
      * <tt>IceTransport</tt> instances, that is
      * {@link #tcpHarvester} and {@link #singlePortHarvesters}.
-     *
-     * @param cfg the {@link ConfigurationService} which provides values to
-     * configurable properties of the behavior/logic of the method
-     * implementation
      */
     public static void initializeStaticConfiguration()
     {
