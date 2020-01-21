d7bdf588d9130d71e102421210cf0da435b80abb
bbaldino <bbaldino@gmail.com>
2020-01-21 02:24:24 -0800

    Remove native code building/assembly (#1040)
    * remove unused native libs
    
    * remove assembly plugin
    
    * bring back assembly plugin to build the zip
    
    this works, but i realized it doesn't support the case where we provide
    a .sh file for every platform (which would still require a bundle per
    platform)
    
    * 2 jvb launch scripts per platform instead of per arch
    
    * include .sh and .bat in archive
    
    * fix logging config arg
    
    * set maven assembly plugin version
    
    * change execution id
    
    * flip skipAssembly default
    
    * use portable file.separator when specifying paths
    
    * remove dist.<platform> targets
    
    these now will do the exact same thing that building the jvb with
    assembly in maven does
    
    * change paths to jvb.sh
    
    * change deb arch to all
    
    * remove outdated dir references
    
    * single deb target
    
    * simplify targets
    
    * remove arch var

--- resources/install/jvb.bat
+++ resources/install/jvb.bat
@@ -9,8 +9,8 @@ goto :begin
 echo Usage:
 echo %0 [OPTIONS], where options can be:
 echo	--secret=SECRET	sets the shared secret used to authenticate to the XMPP server
-echo    --domain=DOMAIN sets the XMPP domain (default: none)
-echo    --host=HOST sets the hostname of the XMPP server (default: domain, if domain is set, localhost otherwise)
+echo 	--domain=DOMAIN	sets the XMPP domain (default: none)
+echo 	--host=HOST	sets the hostname of the XMPP server (default: domain, if domain is set, localhost otherwise)
 echo 	--port=PORT	sets the port of the XMPP server (default: 5275)
 echo    --subdomain=SUBDOMAIN sets the sub-domain used to bind JVB XMPP component (default: jitsi-videobridge)
 echo    --apis=APIS where APIS is a comma separated list of APIs to enable. Currently supported APIs are xmpp and rest. The default is xmpp.
@@ -28,4 +28,4 @@ FOR %%F IN (lib/*.jar) DO (
   SET cp=!cp!;lib/%%F%
 )
 
-java -Djava.library.path=lib/native/windows-64 -Djava.util.logging.config.file=lib/logging.properties -cp %cp% %mainClass% %*
+java -Djava.util.logging.config.file=lib/logging.properties -cp %cp% %mainClass% %*
--- resources/install/jvb.sh
+++ resources/install/jvb.sh
@@ -17,7 +17,6 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 mainClass="org.jitsi.videobridge.Main"
 cp=$SCRIPT_DIR/jitsi-videobridge.jar:$SCRIPT_DIR/lib/*
-libs="$SCRIPT_DIR/lib/native/linux"
 logging_config="$SCRIPT_DIR/lib/logging.properties"
 videobridge_rc="$SCRIPT_DIR/lib/videobridge.rc"
 
@@ -30,4 +29,6 @@ if [ -f $videobridge_rc  ]; then
         source $videobridge_rc
 fi
 
-exec java $VIDEOBRIDGE_DEBUG_OPTIONS -XX:+UseConcMarkSweepGC -Djava.library.path=$libs $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $@
+if [ -z "$VIDEOBRIDGE_MAX_MEMORY" ]; then VIDEOBRIDGE_MAX_MEMORY=3072m; fi
+
+exec java -Xmx$VIDEOBRIDGE_MAX_MEMORY $VIDEOBRIDGE_DEBUG_OPTIONS -XX:+UseConcMarkSweepGC -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $@
