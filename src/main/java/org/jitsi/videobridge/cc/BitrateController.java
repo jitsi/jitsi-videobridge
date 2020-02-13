d470c0c08aaf6fb03f3d4877a8a3707f4d955c10
Jonathan Lennox <jonathan.lennox@8x8.com>
2020-02-13 14:28:19 -0500

    Revert "Filter out the destination endpoint when looping over tracks. (#1090)"
    This reverts commit 34b0d6e4216c7575f9698cd87ee225a95965f5ff.

--- src/main/java/org/jitsi/videobridge/cc/BitrateController.java
+++ src/main/java/org/jitsi/videobridge/cc/BitrateController.java
@@ -431,7 +431,6 @@ public class BitrateController
         long nowMs = System.currentTimeMillis();
         for (MediaStreamTrackDesc sourceTrack : destinationEndpoint
             .getConference().getEndpoints().stream()
-            .filter(e -> !destinationEndpoint.equals(e))
             .map(AbstractEndpoint::getMediaStreamTracks)
             .flatMap(Arrays::stream)
             .filter(t -> t.getRTPEncodings().length > 0)
