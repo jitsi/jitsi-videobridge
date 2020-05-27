jitsi/jitsi-videobridge

commit bb174cce21d2ef4f68496747be4e4828c9c4d8a5

wip: Removes redundant variable and fixes issue described in https://github.com/jitsi/jitsi-videobridge/pull/1260/files.

src/main/java/org/jitsi/videobridge/cc/BitrateController.java
@@ -994,8 +994,6 @@ public class BitrateController
                 ". Endpoints constraints: " + Arrays.toString(videoConstraintsMap.values().toArray()));
         }
 
-        int endpointPriority = 0;
-
         // Before endpoint constraints we had had the notion of selected and
         // pinned endpoints. "selected" endpoints were endpoints that had
         // 720p ideal height and prioritized over everything else and were
@@ -1027,7 +1025,7 @@ public class BitrateController
                 continue;
             }
 
-            boolean forwarded = endpointPriority < adjustedLastN;
+            boolean forwarded = trackBitrateAllocations.size() - 1 < adjustedLastN;
 
             MediaStreamTrackDesc[] tracks
                 = sourceEndpoint.getMediaStreamTracks();
@@ -1037,13 +1035,12 @@ public class BitrateController
                 for (MediaStreamTrackDesc track : tracks)
                 {
                     trackBitrateAllocations.add(
-                        endpointPriority, new TrackBitrateAllocation(
+                        new TrackBitrateAllocation(
                             endpointMultiRank.endpoint.getID(),
                             track, endpointMultiRank.videoConstraints, forwarded));
                 }
 
                 logger.trace(() -> "Adding endpoint " + sourceEndpoint.getID() + " to allocations");
-                endpointPriority++;
             }
         }
 
@@ -1333,13 +1330,13 @@ public class BitrateController
                 if ((lessThanPreferredResolution
                     || (lessThanOrEqualIdealResolution && atLeastPreferredFps)))
                 {
-                    long encodingBitrateBps = encoding.getBitrateBps(nowMs);
-                    if (encodingBitrateBps > 0)
-                    {
-                        idealBps = encodingBitrateBps;
-                    }
-                    ratesList.add(
-                        new RateSnapshot(encodingBitrateBps, encoding));
+                long encodingBitrateBps = encoding.getBitrateBps(nowMs);
+                if (encodingBitrateBps > 0)
+                {
+                    idealBps = encodingBitrateBps;
+                }
+                ratesList.add(
+                    new RateSnapshot(encodingBitrateBps, encoding));
                 }
 
                 if (encoding.getHeight() <= videoConstraints.getPreferredHeight())

commit 41c30724696feccf06d0a7c4598239bb65e39a8c

wip: Fixes bug introduced in bb174cce21d2ef4f68496747be4e4828c9c4d8a5

src/main/java/org/jitsi/videobridge/cc/BitrateController.java
@@ -1025,7 +1025,7 @@ public class BitrateController
                 continue;
             }
 
-            boolean forwarded = trackBitrateAllocations.size() - 1 < adjustedLastN;
+            boolean forwarded = trackBitrateAllocations.size() < adjustedLastN;
 
             MediaStreamTrackDesc[] tracks
                 = sourceEndpoint.getMediaStreamTracks();