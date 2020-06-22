dc80b8b435ddc86454554fd3ba295c7fe1f59dff
Jonathan Lennox <jonathan.lennox@8x8.com>
2020-06-22 17:02:32 -0400

    Add Vp9QualityFilter, skeleton of source projection context.

--- src/main/kotlin/org/jitsi/videobridge/cc/vp9/Vp9AdaptiveSourceProjectionContext.kt
+++ src/main/kotlin/org/jitsi/videobridge/cc/vp9/Vp9AdaptiveSourceProjectionContext.kt
@@ -19,13 +19,15 @@ import org.jitsi.nlj.PacketInfo
 import org.jitsi.nlj.format.PayloadType
 import org.jitsi.nlj.rtp.codec.vp9.Vp9Packet
 import org.jitsi.rtp.rtcp.RtcpSrPacket
+import org.jitsi.rtp.util.RtpUtils.Companion.applyTimestampDelta
+import org.jitsi.rtp.util.RtpUtils.Companion.getTimestampDiff
+import org.jitsi.rtp.util.isNewerThan
 import org.jitsi.utils.logging.DiagnosticContext
 import org.jitsi.utils.logging2.Logger
 import org.jitsi.utils.logging2.createChildLogger
 import org.jitsi.videobridge.cc.AdaptiveSourceProjectionContext
 import org.jitsi.videobridge.cc.RewriteException
 import org.jitsi.videobridge.cc.RtpState
-import org.jitsi.videobridge.cc.vp8.VP8AdaptiveSourceProjectionContext
 import org.json.simple.JSONObject
 
 /**
@@ -41,6 +43,17 @@ class Vp9AdaptiveSourceProjectionContext(
 ) : AdaptiveSourceProjectionContext {
     private val logger: Logger = createChildLogger(parentLogger)
 
+    /**
+     * A map that stores the per-encoding VP9 frame maps.
+     */
+    private val vp9FrameMaps = HashMap<Long, Vp9FrameMap>().withDefault { ssrc: Long? -> Vp9FrameMap(logger) }
+
+    /**
+     * The [Vp9QualityFilter] instance that does quality filtering on the
+     * incoming frames.
+     */
+    private val vp9QualityFilter = Vp9QualityFilter(logger)
+
     private var lastVp9FrameProjection = Vp9FrameProjection(diagnosticContext,
         rtpState.ssrc, rtpState.maxSequenceNumber, rtpState.maxTimestamp)
 
@@ -52,24 +65,192 @@ class Vp9AdaptiveSourceProjectionContext(
     ): Boolean {
         val packet = packetInfo.packet
         if (packet !is Vp9Packet) {
-            logger.warn("Packet is not VP9 packet")
+            logger.warn("Packet is not Vp9 packet")
             return false
         }
 
-        /* TODO */
-        return false
+        val result: Vp9FrameMap.FrameInsertionResult = insertPacketInMap(packet)
+            ?: return false
+            /* Very old frame, more than Vp9FrameMap.FRAME_MAP_SIZE old,
+              or something wrong with the stream. */
+
+        val frame = result.frame
+
+        if (result.isNewFrame) {
+            if (packet.isKeyframe && frameIsNewSsrc(frame)) {
+                /* If we're not currently projecting this SSRC, check if we've
+               already decided to drop a subsequent TL0 frame of this SSRC.
+               If we have, we can't turn on the encoding starting from this
+               packet, so treat this frame as though it weren't a keyframe.
+             */
+                val f: Vp9Frame? = findNextTl0(frame)
+                if (f != null && !f.isAccepted) {
+                    frame.isKeyframe = false
+                }
+            }
+            val receivedMs = packetInfo.receivedTime
+            var accepted: Boolean = vp9QualityFilter
+                .acceptFrame(frame, incomingIndex, targetIndex, receivedMs)
+            if (accepted) {
+                accepted = checkDecodability(frame)
+            }
+            frame.isAccepted = accepted
+            if (accepted) {
+                val projection: Vp9FrameProjection
+                try {
+                    projection = createProjection(frame, packet, result.isReset,
+                        receivedMs)
+                } catch (e: Exception) {
+                    logger.warn("Failed to create frame projection", e)
+                    /* Make sure we don't have an accepted frame without a projection in the map. */
+                    frame.isAccepted = false
+                    return false
+                }
+                frame.projection = projection
+                if (projection.earliestProjectedSeqNum isNewerThan
+                        lastVp9FrameProjection.latestProjectedSeqNum) {
+                    lastVp9FrameProjection = projection
+                }
+            }
+        }
+
+        return frame.isAccepted && frame.projection?.accept(packet) == true
+    }
+
+    /** Lookup a Vp9Frame for a packet. */
+    private fun lookupVp9Frame(vp9Packet: Vp9Packet): Vp9Frame? =
+        vp9FrameMaps[vp9Packet.ssrc]?.findFrame(vp9Packet)
+
+    /**
+     * Insert a packet in the appropriate Vp9FrameMap.
+     */
+    private fun insertPacketInMap(vp9Packet: Vp9Packet) =
+        vp9FrameMaps[vp9Packet.ssrc]?.insertPacket(vp9Packet)
+
+    /**
+     * Find the previous frame before the given one.
+     */
+    @Synchronized
+    private fun prevFrame(frame: Vp9Frame) =
+        vp9FrameMaps[frame.ssrc]?.prevFrame(frame)
+
+    /**
+     * Find the next frame after the given one.
+     */
+    @Synchronized
+    private fun nextFrame(frame: Vp9Frame) =
+        vp9FrameMaps[frame.ssrc]?.nextFrame(frame)
+
+    /**
+     * Find the previous accepted frame before the given one.
+     */
+    private fun findPrevAcceptedFrame(frame: Vp9Frame) =
+        vp9FrameMaps[frame.ssrc]?.findPrevAcceptedFrame(frame)
+
+    /**
+     * Find the next accepted frame after the given one.
+     */
+    private fun findNextAcceptedFrame(frame: Vp9Frame) =
+        vp9FrameMaps[frame.ssrc]?.findNextAcceptedFrame(frame)
+
+    /**
+     * Find a subsequent Tid==0 frame after the given frame
+     * @param frame The frame to query
+     * @return A subsequent TL0 frame, or null
+     */
+    private fun findNextTl0(frame: Vp9Frame) =
+        vp9FrameMaps[frame.ssrc]?.findNextTl0(frame)
+
+    private fun frameIsNewSsrc(frame: Vp9Frame): Boolean {
+        val lastFrame = lastVp9FrameProjection.vp9Frame
+
+        return lastFrame != null && frame.matchesSSRC(lastFrame)
+    }
+
+    /**
+     * For a frame that's been accepted by the quality filter, verify that
+     * it's decodable given the projection decisions about previous frames
+     * (in case the targetIndex has changed).
+     */
+    private fun checkDecodability(frame: Vp9Frame): Boolean {
+        /* TODO - use SS or flexible mode reference list */
+        return true
+    }
+
+    /**
+     * Create a projection for this frame.
+     */
+    private fun createProjection(
+        frame: Vp9Frame,
+        initialPacket: Vp9Packet,
+        isReset: Boolean,
+        receivedMs: Long
+    ): Vp9FrameProjection {
+        /* TODO - rewriting */
+        return Vp9FrameProjection(
+            diagnosticContext = diagnosticContext,
+            vp9Frame = frame,
+            ssrc = lastVp9FrameProjection.ssrc,
+            timestamp = frame.timestamp,
+            sequenceNumberDelta = 0,
+            pictureId = frame.pictureId,
+            tl0PICIDX = frame.tl0PICIDX,
+            createdMs = receivedMs
+        )
     }
 
     override fun needsKeyframe(): Boolean {
-        return false
+        if (vp9QualityFilter.needsKeyframe()) {
+            return true
+        }
+
+        return lastVp9FrameProjection.vp9Frame == null
     }
 
     @Throws(RewriteException::class)
     override fun rewriteRtp(packetInfo: PacketInfo) {
+        if (packetInfo.packet !is Vp9Packet) {
+            logger.info("Got a non-VP9 packet.")
+            throw RewriteException("Non-VP9 packet in VP9 source projection")
+        }
+        val vp9Packet = packetInfo.packetAs<Vp9Packet>()
+
+        if (vp9Packet.pictureId == -1) {
+            /* Should have been routed to generic projection context. */
+            logger.info("VP9 packet does not have picture ID, cannot track in frame map.")
+            throw RewriteException("VP9 packet without picture ID in VP9 source projection")
+        }
+
+        val vp9Frame: Vp9Frame = lookupVp9Frame(vp9Packet)
+            ?: throw RewriteException("Frame not in tracker (aged off?)")
+
+        val vp9Projection = vp9Frame.projection
+            ?: throw RewriteException("Frame does not have projection?")
+            /* Shouldn't happen for an accepted packet whose frame is still known? */
+
+        vp9Projection.rewriteRtp(vp9Packet)
     }
 
     override fun rewriteRtcp(rtcpSrPacket: RtcpSrPacket): Boolean {
-        return false
+        val lastVp9FrameProjectionCopy: Vp9FrameProjection = lastVp9FrameProjection
+        if (rtcpSrPacket.senderSsrc != lastVp9FrameProjectionCopy.vp9Frame?.ssrc) {
+            return false
+        }
+
+        rtcpSrPacket.senderSsrc = lastVp9FrameProjectionCopy.ssrc
+
+        val srcTs = rtcpSrPacket.senderInfo.rtpTimestamp
+        val delta = getTimestampDiff(
+            lastVp9FrameProjectionCopy.timestamp,
+            lastVp9FrameProjectionCopy.vp9Frame.timestamp)
+
+        val dstTs = applyTimestampDelta(srcTs, delta)
+
+        if (srcTs != dstTs) {
+            rtcpSrPacket.senderInfo.rtpTimestamp = dstTs
+        }
+
+        return true
     }
 
     override fun getRtpState() = RtpState(
@@ -84,7 +265,7 @@ class Vp9AdaptiveSourceProjectionContext(
     @Synchronized
     override fun getDebugState(): JSONObject {
         val debugState = JSONObject()
-        debugState["class"] = VP8AdaptiveSourceProjectionContext::class.java.simpleName
+        debugState["class"] = Vp9AdaptiveSourceProjectionContext::class.java.simpleName
 
         /* TODO */
 
---
+++ src/main/kotlin/org/jitsi/videobridge/cc/vp9/Vp9QualityFilter.kt
@@ -0,0 +1,317 @@
+/*
+ * Copyright @ 2019 8x8, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.videobridge.cc.vp9
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
+import org.jitsi.nlj.RtpLayerDesc.Companion.getEidFromIndex
+import org.jitsi.nlj.RtpLayerDesc.Companion.getTidFromIndex
+import org.jitsi.utils.logging2.Logger
+import org.jitsi.utils.logging2.createChildLogger
+import org.json.simple.JSONObject
+
+/**
+ * This class is responsible for dropping VP9 simulcast/svc packets based on
+ * their quality, i.e. packets that correspond to qualities that are above a
+ * given quality target. Instances of this class are thread-safe.
+ */
+internal class Vp9QualityFilter(parentLogger: Logger) {
+    /**
+     * The [Logger] to be used by this instance to print debug
+     * information.
+     */
+    private val logger: Logger = createChildLogger(parentLogger)
+
+    /**
+     * Holds the arrival time (in millis) of the most recent keyframe group.
+     * Reading/writing of this field is synchronized on this instance.
+     */
+    private var mostRecentKeyframeGroupArrivalTimeMs = -1L
+
+    /**
+     * A boolean flag that indicates whether a simulcast switch is pending. This
+     * condition is equivalent to:
+     *
+     * internalSpatialLayerIdTarget != externalSpatialLayerIdTarget.
+     *
+     * Reading/writing of this field is synchronized on this instance.
+     */
+    private var needsKeyframe = false
+
+    /**
+     * The encoding id that this instance tries to achieve. Upon
+     * receipt of a packet, we check whether externalSpatialLayerIdTarget
+     * (that's specified as an argument to the
+     * [acceptFrame] method) is set to something
+     * different, in which case we set [needsKeyframe] equal to true and
+     * update.
+     */
+    private var internalEncodingIdTarget = SUSPENDED_ENCODING_ID
+
+    /**
+     * The encoding layer ID that we're currently forwarding. -1
+     * indicates that we're not forwarding anything. Reading/writing of this
+     * field is synchronized on this instance.
+     */
+    private var currentEncodingId = SUSPENDED_ENCODING_ID
+
+    /**
+     * @return true if a the target encoding id has changed and a
+     * keyframe hasn't been received yet, false otherwise.
+     */
+    fun needsKeyframe(): Boolean {
+        return needsKeyframe
+    }
+
+    /**
+     * Determines whether to accept or drop a VP9 frame.
+     *
+     * Note that, at the time of this writing, there's no practical need for a
+     * synchronized keyword because there's only one thread accessing this
+     * method at a time.
+     *
+     * @param frame the VP9 frame.
+     * @param incomingIndex the quality index of the incoming RTP packet
+     * @param externalTargetIndex the target quality index that the user of this
+     * instance wants to achieve.
+     * @param receivedMs the current time (in millis)
+     * @return true to accept the VP9 frame, otherwise false.
+     */
+    @Synchronized
+    fun acceptFrame(
+        frame: Vp9Frame,
+        incomingIndex: Int,
+        externalTargetIndex: Int,
+        receivedMs: Long
+    ): Boolean {
+        // We make local copies of the externalTemporalLayerIdTarget and the
+        // externalEncodingTarget (as they may be updated by some other
+        // thread).
+        val externalTemporalLayerIdTarget = getTidFromIndex(externalTargetIndex)
+        val externalEncodingIdTarget = getEidFromIndex(externalTargetIndex)
+        if (externalEncodingIdTarget != internalEncodingIdTarget) {
+            // The externalEncodingIdTarget has changed since accept last
+            // run; perhaps we should request a keyframe.
+            internalEncodingIdTarget = externalEncodingIdTarget
+            if (externalEncodingIdTarget > SUSPENDED_ENCODING_ID) {
+                needsKeyframe = true
+            }
+        }
+        if (externalEncodingIdTarget < 0 || externalTemporalLayerIdTarget < 0) {
+            // We stop forwarding immediately. We will need a keyframe in order
+            // to resume.
+            currentEncodingId = SUSPENDED_ENCODING_ID
+            return false
+        }
+        var temporalLayerIdOfFrame = frame.temporalLayer
+        if (temporalLayerIdOfFrame < 0) {
+            // temporal scalability is not enabled. Pretend that
+            // this is the base temporal layer.
+            temporalLayerIdOfFrame = 0
+        }
+        val encodingId = getEidFromIndex(incomingIndex)
+        return if (frame.isKeyframe) {
+            logger.debug {
+                "Quality filter got keyframe for stream ${frame.ssrc}"
+            }
+            acceptKeyframe(encodingId, receivedMs)
+        } else if (currentEncodingId > SUSPENDED_ENCODING_ID) {
+            if (isOutOfSwitchingPhase(receivedMs) && isPossibleToSwitch(encodingId)) {
+                // XXX(george) i've noticed some "rogue" base layer keyframes
+                // that trigger this. what happens is the client sends a base
+                // layer key frame, the bridge switches to that layer because
+                // for all it knows it may be the only keyframe sent by the
+                // client engine. then the bridge notices that packets from the
+                // higher quality streams are flowing and execution ends-up
+                // here. it is a mystery why the engine is "leaking" base layer
+                // key frames
+                needsKeyframe = true
+            }
+            if (encodingId != currentEncodingId) {
+                // for non-keyframes, we can't route anything but the current encoding
+                return false
+            }
+
+            // This branch reads the {@link #currentEncodingId} and it
+            // filters packets based on their temporal layer.
+            if (currentEncodingId > externalEncodingIdTarget) {
+                // pending downscale, decrease the frame rate until we
+                // downscale.
+                temporalLayerIdOfFrame < 1
+            } else if (currentEncodingId < externalEncodingIdTarget) {
+                // pending upscale, increase the frame rate until we upscale.
+                true
+            } else {
+                // The currentSpatialLayerId matches exactly the target
+                // currentSpatialLayerId.
+                temporalLayerIdOfFrame <= externalTemporalLayerIdTarget
+            }
+        } else {
+            // In this branch we're not processing a keyframe and the
+            // currentSpatialLayerId is in suspended state, which means we need
+            // a keyframe to start streaming again. Reaching this point also
+            // means that we want to forward something (because both
+            // externalEncodingIdTarget and externalTemporalLayerIdTarget
+            // are greater than 0) so we set the request keyframe flag.
+
+            // assert needsKeyframe == true;
+            false
+        }
+    }
+
+    /**
+     * Returns a boolean that indicates whether we are in layer switching phase
+     * or not.
+     *
+     * @param receivedMs the time the latest frame was received (in millis)
+     * @return true if we're in layer switching phase, false otherwise.
+     */
+    @Synchronized
+    private fun isOutOfSwitchingPhase(receivedMs: Long): Boolean {
+        val deltaMs = receivedMs - mostRecentKeyframeGroupArrivalTimeMs
+        return deltaMs > MIN_KEY_FRAME_WAIT_MS
+    }
+
+    /**
+     * @return true if it looks like we can re-scale (see implementation of
+     * method for specific details).
+     */
+    @Synchronized
+    private fun isPossibleToSwitch(encodingId: Int): Boolean {
+        if (encodingId == -1) {
+            // We failed to resolve the spatial/quality layer of the packet.
+            return false
+        }
+        return if (encodingId > currentEncodingId &&
+            currentEncodingId < internalEncodingIdTarget) {
+            // It looks like upscaling is possible.
+            true
+        } else if (encodingId < currentEncodingId &&
+            currentEncodingId > internalEncodingIdTarget) {
+            // It looks like downscaling is possible.
+            true
+        } else {
+            false
+        }
+    }
+
+    /**
+     * Determines whether to accept or drop a VP9 keyframe. This method updates
+     * the spatial layer id.
+     *
+     * Note that, at the time of this writing, there's no practical need for a
+     * synchronized keyword because there's only one thread accessing this
+     * method at a time.
+     *
+     * @param receivedMs the time the frame was received (in millis)
+     * @return true to accept the VP9 keyframe, otherwise false.
+     */
+    @Synchronized
+    private fun acceptKeyframe(
+        encodingIdOfKeyframe: Int,
+        receivedMs: Long
+    ): Boolean {
+        // This branch writes the {@link #currentSpatialLayerId} and it
+        // determines whether or not we should switch to another simulcast
+        // stream.
+        if (encodingIdOfKeyframe < 0) {
+            // something went terribly wrong, normally we should be able to
+            // extract the layer id from a keyframe.
+            logger.error("unable to get layer id from keyframe")
+            return false
+        }
+        logger.debug {
+            "Received a keyframe of encoding: $encodingIdOfKeyframe"
+        }
+
+        // The keyframe request has been fulfilled at this point, regardless of
+        // whether we'll be able to achieve the internalEncodingIdTarget.
+        needsKeyframe = false
+        return if (isOutOfSwitchingPhase(receivedMs)) {
+            // During the switching phase we always project the first
+            // keyframe because it may very well be the only one that we
+            // receive (i.e. the endpoint is sending low quality only). Then
+            // we try to approach the target.
+            mostRecentKeyframeGroupArrivalTimeMs = receivedMs
+            logger.debug {
+                "First keyframe in this kf group " +
+                    "currentEncodingId: $encodingIdOfKeyframe. " +
+                    "Target is $internalEncodingIdTarget"
+            }
+            if (encodingIdOfKeyframe <= internalEncodingIdTarget) {
+                // If the target is 180p and the first keyframe of a group of
+                // keyframes is a 720p keyframe we don't project it. If we
+                // receive a 720p keyframe, we know that there MUST be a 180p
+                // keyframe shortly after.
+                currentEncodingId = encodingIdOfKeyframe
+                true
+            } else {
+                false
+            }
+        } else {
+            // We're within the 300ms window since the reception of the
+            // first key frame of a key frame group, let's check whether an
+            // upscale/downscale is possible.
+            if (encodingIdOfKeyframe in currentEncodingId..internalEncodingIdTarget) {
+                // upscale or current quality case
+                currentEncodingId = encodingIdOfKeyframe
+                logger.debug {
+                    "Upscaling to encoding $encodingIdOfKeyframe. " +
+                        "The target is $internalEncodingIdTarget"
+                }
+                true
+            } else if (internalEncodingIdTarget in encodingIdOfKeyframe until currentEncodingId) {
+                // downscale case
+                currentEncodingId = encodingIdOfKeyframe
+                logger.debug {
+                    "Downscaling to encoding $encodingIdOfKeyframe. " +
+                        "The target is $internalEncodingIdTarget"
+                }
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    /**
+     * Gets a JSON representation of the parts of this object's state that
+     * are deemed useful for debugging.
+     */
+    @get:SuppressFBWarnings(value = ["IS2_INCONSISTENT_SYNC"], justification = "We intentionally avoid synchronizing while reading" +
+        " fields only used in debug output.")
+    val debugState: JSONObject
+        get() {
+            val debugState = JSONObject()
+            debugState["mostRecentKeyframeGroupArrivalTimeMs"] = mostRecentKeyframeGroupArrivalTimeMs
+            debugState["needsKeyframe"] = needsKeyframe
+            debugState["internalEncodingIdTarget"] = internalEncodingIdTarget
+            debugState["currentEncodingId"] = currentEncodingId
+            return debugState
+        }
+
+    companion object {
+        /**
+         * The default maximum frequency (in millis) at which the media engine
+         * generates key frame.
+         */
+        private const val MIN_KEY_FRAME_WAIT_MS = 300
+
+        /**
+         * The HD, SD, LD and suspended spatial/quality layer IDs.
+         */
+        private const val SUSPENDED_ENCODING_ID = -1
+    }
+}
