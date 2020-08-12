e3d0f5eb24a8b25bb9da1e1e98159deec64443f1
Boris Grozev <boris@jitsi.org>
2020-08-11 19:37:58 -0500

    feat: Add a 301 redirect for /colibri/debug -> /debug.

---
+++ jvb/src/main/java/org/jitsi/videobridge/rest/root/colibri/debug/Debug.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.videobridge.rest.root.colibri.debug;
+
+import org.jitsi.videobridge.rest.root.colibri.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response;
+import java.net.*;
+
+/**
+ * We moved /colibri/debug to /debug.
+ */
+@Path("/colibri/debug")
+public class Debug extends ColibriResource
+{
+    @POST
+    @Path("{path}")
+    public Response post(@PathParam("path") String path, @Context UriInfo uriInfo)
+    {
+        return create301(path, uriInfo);
+    }
+
+    @GET
+    @Path("{path}")
+    public Response get(@PathParam("path") String path, @Context UriInfo uriInfo)
+    {
+        return create301(path, uriInfo);
+    }
+
+    private Response create301(String path, UriInfo uriInfo)
+    {
+        String target = uriInfo.getBaseUri() + "debug/" + path;
+        if (uriInfo.getQueryParameters().get("full") != null)
+        {
+            target += "?full=" + uriInfo.getQueryParameters().get("full");
+        }
+        return Response.status(301).location(URI.create(target)).build();
+    }
+}
