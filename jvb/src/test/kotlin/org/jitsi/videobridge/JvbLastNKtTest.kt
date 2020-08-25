52eb05c8a5411dfb7380874637d00fc70d4b2732
bbaldino <bbaldino@gmail.com>
2020-08-25 11:04:23 -0700

    JVB-wide last-n value (#1398)
    * remove unneeded static member
    
    * add jvb-wide last-n setting
    
    * take jvb last-n setting into account in BitrateController
    
    * simplify calculateLastN logic

---
+++ jvb/src/test/kotlin/org/jitsi/videobridge/JvbLastNKtTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jitsi.videobridge
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+
+class JvbLastNKtTest : ShouldSpec({
+    context("calculateLastN") {
+        should("return the correct result") {
+            calculateLastN(-1, -1) shouldBe -1
+            calculateLastN(-1, 10) shouldBe 10
+            calculateLastN(10, -1) shouldBe 10
+            calculateLastN(2, 3) shouldBe 2
+        }
+    }
+})
