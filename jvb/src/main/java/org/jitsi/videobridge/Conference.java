0f229fc0fd5bbd2b4a421b96077138c352125459
damencho <damencho@jitsi.org>
2020-09-04 09:34:30 -0500

    fix: Fixes dominant speaker change log print.

--- jvb/src/main/java/org/jitsi/videobridge/Conference.java
+++ jvb/src/main/java/org/jitsi/videobridge/Conference.java
@@ -456,7 +456,7 @@ public class Conference
 
         if (logger.isInfoEnabled())
         {
-            logger.info("ds_change ds_id=" + id);
+            logger.info("ds_change ds_id=" + dominantSpeakerId);
             getVideobridge().getStatistics().totalDominantSpeakerChanges.increment();
         }
 
