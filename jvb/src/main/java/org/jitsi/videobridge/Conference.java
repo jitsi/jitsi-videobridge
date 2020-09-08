def683d45c2e04530d0c04f69ea9e582d915cd87
Brian Baldino <bbaldino@gmail.com>
2020-09-08 10:10:55 -0700

    wrap all epconnectionstatusmonitor uses with null checks

--- jvb/src/main/java/org/jitsi/videobridge/Conference.java
+++ jvb/src/main/java/org/jitsi/videobridge/Conference.java
@@ -854,7 +854,10 @@ public class Conference
 
         if (removedEndpoint != null)
         {
-            epConnectionStatusMonitor.endpointExpired(removedEndpoint.getID());
+            if (epConnectionStatusMonitor != null)
+            {
+                epConnectionStatusMonitor.endpointExpired(removedEndpoint.getID());
+            }
             final EventAdmin eventAdmin = getEventAdmin();
             if (eventAdmin != null)
             {
@@ -907,7 +910,10 @@ public class Conference
         {
             eventAdmin.postEvent(EventFactory.endpointMessageTransportReady(endpoint));
         }
-        epConnectionStatusMonitor.endpointConnected(endpoint.getID());
+        if (epConnectionStatusMonitor != null)
+        {
+            epConnectionStatusMonitor.endpointConnected(endpoint.getID());
+        }
 
         if (!isExpired())
         {
