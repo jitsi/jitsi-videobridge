2f0a73d836c63f790695c68e43124b58c9001392
Shoichi Hasegawa <hasevr@gmail.com>
2020-09-22 17:58:48 +0900

    Add scripts to copy and update docker's jvb

---
+++ jvb/copyClasses.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp -a /c/Home/hase/debian/jitsi-videobridge/jvb/target/classes /c/Users/hase/.jitsi-meet-cfg/myjvb/
---
+++ jvb/makeLink.bat
@@ -0,0 +1,1 @@
+mklink /J C:\Users\hase\.jitsi-meet-cfg\myjvb\jitsi-videobridge-2.1-SNAPSHOT\classes C:\Home\hase\debian\jitsi-videobridge\jvb\target\classes
