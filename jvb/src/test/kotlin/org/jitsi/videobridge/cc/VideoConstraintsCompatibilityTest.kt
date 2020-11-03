fa8a317bbe5c80ca522ec8f18c16b04286fece69
Boris Grozev <boris@jitsi.org>
2020-11-03 14:07:14 -0600

    test: Add tests for VideoConstraintsCompatibility.

---
+++ jvb/src/test/kotlin/org/jitsi/videobridge/cc/VideoConstraintsCompatibilityTest.kt
@@ -0,0 +1,28 @@
+package org.jitsi.videobridge.cc
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.maps.shouldContainExactly
+import org.jitsi.videobridge.VideoConstraints
+
+class VideoConstraintsCompatibilityTest : ShouldSpec() {
+    init {
+        val vcc = VideoConstraintsCompatibility()
+        context("Stage view behavior") {
+            vcc.setMaxFrameHeight(720)
+            vcc.setSelectedEndpoints(setOf("A"))
+            vcc.computeVideoConstraints().shouldContainExactly(mapOf(
+                "A" to VideoConstraints(720, 360, 30.0)
+            ))
+        }
+        context("Tile view behavior") {
+            vcc.setMaxFrameHeight(180)
+            vcc.setSelectedEndpoints(setOf("A", "B", "C", "D"))
+            vcc.computeVideoConstraints().shouldContainExactly(mapOf(
+                "A" to VideoConstraints(180, -1, -1.0),
+                "B" to VideoConstraints(180, -1, -1.0),
+                "C" to VideoConstraints(180, -1, -1.0),
+                "D" to VideoConstraints(180, -1, -1.0)
+            ))
+        }
+    }
+}
