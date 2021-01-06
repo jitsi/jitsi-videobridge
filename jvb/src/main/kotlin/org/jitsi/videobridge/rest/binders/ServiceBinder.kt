9dd0ec1e9c71acbb2c69061286aee6e36c0bb39e
Boris Grozev <boris@jitsi.org>
2021-01-06 16:38:00 -0600

    ref: Adapt to jicoco changes.

--- jvb/src/main/kotlin/org/jitsi/videobridge/rest/binders/ServiceBinder.kt
+++ jvb/src/main/kotlin/org/jitsi/videobridge/rest/binders/ServiceBinder.kt
@@ -17,22 +17,16 @@
 package org.jitsi.videobridge.rest.binders
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder
-import org.jitsi.health.HealthCheckServiceSupplier
-import org.jitsi.utils.version.VersionService
-import org.jitsi.version.VersionServiceSupplier
+import org.jitsi.health.HealthCheckService
 import org.jitsi.videobridge.Videobridge
-import org.jitsi.videobridge.health.JvbHealthCheckServiceSupplier
-import org.jitsi.videobridge.health.JvbHealthChecker
 import org.jitsi.videobridge.stats.StatsCollector
-import org.jitsi.videobridge.version.JvbVersionServiceSupplier
 import org.jitsi.videobridge.xmpp.XmppConnection
 
 class ServiceBinder(
     private val videobridge: Videobridge,
     private val xmppConnection: XmppConnection,
     private val statsCollector: StatsCollector?,
-    private val versionService: VersionService,
-    private val healthChecker: JvbHealthChecker
+    private val healthChecker: HealthCheckService
 ) : AbstractBinder() {
     override fun configure() {
         bind(videobridge).to(Videobridge::class.java)
@@ -42,9 +36,6 @@ class ServiceBinder(
             bind(statsCollector).to(StatsCollector::class.java)
         }
         bind(xmppConnection).to(XmppConnection::class.java)
-        // These are still suppliers rather than direct instances because that's what
-        // Jicoco requires
-        bind(JvbHealthCheckServiceSupplier(healthChecker)).to(HealthCheckServiceSupplier::class.java)
-        bind(JvbVersionServiceSupplier(versionService)).to(VersionServiceSupplier::class.java)
+        bind(healthChecker).to(HealthCheckService::class.java)
     }
 }
