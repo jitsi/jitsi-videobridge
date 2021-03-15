a658097f0a4f3aa0783571578fa2266b0aa07333
Jonathan Lennox <jonathan.lennox@8x8.com>
2021-03-15 14:00:35 -0400

    Argument to BitrateController#accept(RtcpSrPacket) shouldn't be nullable. (#1608)

--- jvb/src/main/kotlin/org/jitsi/videobridge/cc/allocation/BitrateController.kt
+++ jvb/src/main/kotlin/org/jitsi/videobridge/cc/allocation/BitrateController.kt
@@ -139,7 +139,7 @@ class BitrateController<T : MediaSourceContainer> @JvmOverloads constructor(
 
     // Proxy to the packet handler
     fun accept(packetInfo: PacketInfo): Boolean = packetHandler.accept(packetInfo)
-    fun accept(rtcpSrPacket: RtcpSrPacket?): Boolean {
+    fun accept(rtcpSrPacket: RtcpSrPacket): Boolean {
         // TODO: It is not clear why this is here, and why it isn't in the other accept() method.
         bandwidthAllocator.maybeUpdate()
 
