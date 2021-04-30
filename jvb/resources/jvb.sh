5bf404854bd73631e3afa1c3e362278555380adc
Jonathan Lennox <jonathan.lennox@8x8.com>
2021-04-30 10:36:59 -0400

    Use G1GC by default on Java 11+. (#1642)

--- jvb/resources/jvb.sh
+++ jvb/resources/jvb.sh
@@ -25,6 +25,18 @@ if [ -f $videobridge_rc  ]; then
 fi
 
 if [ -z "$VIDEOBRIDGE_MAX_MEMORY" ]; then VIDEOBRIDGE_MAX_MEMORY=3072m; fi
-if [ -z "$VIDEOBRIDGE_GC_TYPE" ]; then VIDEOBRIDGE_GC_TYPE=ConcMarkSweepGC; fi
+if [ -z "$VIDEOBRIDGE_GC_TYPE" ]; then
+    JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    # Experiments indicate ConcMarkSweepGC is better for JVM in Java 1.8;
+    # G1GC is better in later Java versions.  (This only considers currently-supported versions.)
+    case $JAVA_VERSION in
+        1.8*)
+            VIDEOBRIDGE_GC_TYPE=ConcMarkSweepGC;
+            ;;
+        *)
+            VIDEOBRIDGE_GC_TYPE=G1GC;
+            ;;
+    esac
+fi
 
 exec java -Xmx$VIDEOBRIDGE_MAX_MEMORY $VIDEOBRIDGE_DEBUG_OPTIONS -XX:+Use$VIDEOBRIDGE_GC_TYPE -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp -Djdk.tls.ephemeralDHKeySize=2048 $LOGGING_CONFIG_PARAM $JAVA_SYS_PROPS -cp $cp $mainClass $@
