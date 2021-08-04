33d976cb3bdc201735a7ba3da937d684347644db
Boris Grozev <boris@jitsi.org>
2021-08-04 16:23:20 -0500

    squash: Fix broken compilation.

--- jvb/src/test/kotlin/org/jitsi/videobridge/rest/root/colibri/stats/StatsTest.kt
+++ jvb/src/test/kotlin/org/jitsi/videobridge/rest/root/colibri/stats/StatsTest.kt
@@ -67,5 +67,6 @@ class StatsTest : JerseyTest() {
     private fun Response.getResultAsJson(): JSONObject {
         val obj = JSONParser().parse(readEntity(String::class.java))
         obj.shouldBeInstanceOf<JSONObject>()
+        return obj
     }
 }
