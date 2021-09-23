85b816d1596d4ae08e89d971348e55ca99f162b0
Boris Grozev <boris@jitsi.org>
2021-09-23 09:37:33 -0500

    fix: Send dominant speaker event only when there are recent speakers.

--- jvb/src/main/java/org/jitsi/videobridge/ConferenceSpeechActivity.java
+++ jvb/src/main/java/org/jitsi/videobridge/ConferenceSpeechActivity.java
@@ -239,6 +239,7 @@ public class ConferenceSpeechActivity
     /**
      * Get a list of recent speakers (including the current dominant one at the top of the list).
      */
+    @NotNull
     public List<String> getRecentSpeakers()
     {
         return recentSpeakers.getRecentSpeakers().stream()
