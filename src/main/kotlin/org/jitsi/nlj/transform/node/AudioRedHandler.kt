d45448b09e3979ad847d6feb8ef43d9f99bfe2dc
bgrozev <boris@jitsi.org>
2021-12-13 11:06:39 -0700

    fix(red): Do not encode packets with an invalid timestamp offset (#385)
    * fix(red): Do not encode packets with an invalid timestamp offset
    
    As discovered by @fippo, using opus with DTX results in 400ms frames
    which is too large to be encoded. Do not attempt to encode such packets.

--- src/main/kotlin/org/jitsi/nlj/transform/node/AudioRedHandler.kt
+++ src/main/kotlin/org/jitsi/nlj/transform/node/AudioRedHandler.kt
@@ -27,9 +27,11 @@ import org.jitsi.nlj.stats.NodeStatsBlock
 import org.jitsi.nlj.util.BufferPool
 import org.jitsi.nlj.util.ReadOnlyStreamInformationStore
 import org.jitsi.nlj.util.RtpPacketCache
+import org.jitsi.rtp.rtp.RedundancyBlockHeader
 import org.jitsi.rtp.rtp.RtpPacket
 import org.jitsi.rtp.rtp.header_extensions.AudioLevelHeaderExtension
 import org.jitsi.rtp.util.RtpUtils.Companion.applySequenceNumberDelta
+import org.jitsi.rtp.util.RtpUtils.Companion.getTimestampDiffAsInt
 
 class AudioRedHandler(
     streamInformationStore: ReadOnlyStreamInformationStore
@@ -123,20 +125,32 @@ class AudioRedHandler(
 
             when (config.distance) {
                 RedDistance.ONE -> {
-                    getPacketToProtect(applySequenceNumberDelta(seq, -1), config.vadOnly)?.also { secondary ->
+                    getPacketToProtect(
+                        applySequenceNumberDelta(seq, -1),
+                        audioRtpPacket.timestamp,
+                        config.vadOnly
+                    )?.also { secondary ->
                         redundancy.add(secondary)
                         stats.redundancyPacketAdded()
                     }
                 }
                 RedDistance.TWO -> {
-                    getPacketToProtect(applySequenceNumberDelta(seq, -1), false)?.also { secondary ->
+                    getPacketToProtect(
+                        applySequenceNumberDelta(seq, -1),
+                        audioRtpPacket.timestamp,
+                        false
+                    )?.also { secondary ->
                         // With distance 2 we only add the tertiary packet when there is a secondary available
                         // (regardless of secondary's VAD). This guarantees that the sequence numbers of the
                         // redundancy packets always directly proceed the primary packet, i.e. that we don't encode
                         // a packet with primary seq=N and a single redundancy with seq=N-2. This is be important
                         // when the receiver of the RED stream is another jitsi-videobridge instance (via Octo),
                         // which makes that assumption about the stream it receives.
-                        val tertiary = getPacketToProtect(applySequenceNumberDelta(seq, -2), config.vadOnly)
+                        val tertiary = getPacketToProtect(
+                            applySequenceNumberDelta(seq, -2),
+                            audioRtpPacket.timestamp,
+                            config.vadOnly
+                        )
                         if (tertiary != null) {
                             redundancy.add(tertiary)
                             stats.redundancyPacketAdded()
@@ -162,14 +176,20 @@ class AudioRedHandler(
             return packetInfo
         }
 
-        private fun getPacketToProtect(seq: Int, vadOnly: Boolean): RtpPacket? {
+        private fun getPacketToProtect(seq: Int, primaryTimestamp: Long, vadOnly: Boolean): RtpPacket? {
             // All of the transform pipeline runs in a single thread, and we only use the packet momentarily to make a
-            // copy into a new RED packet, so its safe to just peek() at it.
+            // copy into a new RED packet, so it's safe to just peek() at it.
             sentAudioCache.peek(seq)?.item?.let {
                 // In vad-only mode, we only add redundancy for packets that have an audio level extension with the VAD
                 // bit set.
                 if (!vadOnly || it.hasVad()) {
-                    return it
+                    // Don't attempt to encode packets with timestamp diff that's too large to encode (happens with
+                    // 400ms opus frames e.g. when DTX is used)
+                    if (getTimestampDiffAsInt(primaryTimestamp, it.timestamp) <=
+                        RedundancyBlockHeader.MAX_TIMESTAMP_OFFSET
+                    ) {
+                        return it
+                    }
                 }
             }
             return null
