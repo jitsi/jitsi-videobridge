9b8de3c439696efe9d69661ba62a81eb93a4d47a
Boris Grozev <boris@jitsi.org>
2021-12-29 16:32:53 -0600

    fix: Set min/max/preferred ports.

--- jvb/src/main/kotlin/org/jitsi/videobridge/transport/ice/IceTransport.kt
+++ jvb/src/main/kotlin/org/jitsi/videobridge/transport/ice/IceTransport.kt
@@ -130,9 +130,9 @@ class IceTransport @JvmOverloads constructor(
 
     private val iceComponent = iceAgent.createComponent(
         iceStream,
-        -1,
-        -1,
-        -1,
+        11111,
+        1024,
+        65535,
         IceConfig.config.keepAliveStrategy,
         IceConfig.config.useComponentSocket
     )
