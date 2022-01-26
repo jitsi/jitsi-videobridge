3f66a1f58bb710d82a3fdffc00fcce9d8d66c43a
Jonathan Lennox <jonathan.lennox@8x8.com>
2022-01-26 14:23:01 -0500

    Convert PacketTransitStats to Kotlin.

---
+++ jvb/src/main/kotlin/org/jitsi/videobridge/stats/PacketTransitStats.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright @ 2018 - present 8x8, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jitsi.videobridge.stats
+
+import org.jitsi.utils.OrderedJsonObject
+import org.jitsi.videobridge.Endpoint
+import org.jitsi.videobridge.Endpoint.Companion.getPacketDelayStats
+import org.jitsi.videobridge.relay.Relay
+import org.jitsi.videobridge.relay.Relay.Companion.getPacketDelayStats
+
+object PacketTransitStats {
+    @JvmStatic
+    val statsJson: OrderedJsonObject
+        get() {
+            val stats = OrderedJsonObject()
+            stats["e2e_packet_delay"] = Endpoint.getPacketDelayStats()
+            stats["relay_e2e_packet_delay"] = Relay.getPacketDelayStats()
+            stats[Endpoint.overallAverageBridgeJitter.name] = Endpoint.overallAverageBridgeJitter.get()
+            return stats
+        }
+}
