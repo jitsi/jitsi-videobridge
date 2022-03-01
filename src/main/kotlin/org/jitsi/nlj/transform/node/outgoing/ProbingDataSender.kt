47ec33711eb76ed02466af0328e990651e74c93f
Boris Grozev <boris@jitsi.org>
2022-03-01 17:15:58 -0600

    fix: Fix a ConcurrentModificationException
    java.util.ConcurrentModificationException
            at java.base/java.util.LinkedHashMap$LinkedHashIterator.nextNode(LinkedHashMap.java:719)
            at java.base/java.util.LinkedHashMap$LinkedKeyIterator.next(LinkedHashMap.java:741)
            at java.base/java.util.AbstractCollection.toString(AbstractCollection.java:472)
            at org.jitsi.nlj.transform.node.outgoing.ProbingDataSender.getNodeStats(ProbingDataSender.kt:193)
            at org.jitsi.nlj.RtpSenderImpl.getNodeStats(RtpSenderImpl.kt:298)

--- src/main/kotlin/org/jitsi/nlj/transform/node/outgoing/ProbingDataSender.kt
+++ src/main/kotlin/org/jitsi/nlj/transform/node/outgoing/ProbingDataSender.kt
@@ -37,6 +37,7 @@ import org.jitsi.utils.logging.TimeSeriesLogger
 import org.jitsi.utils.logging2.Logger
 import org.jitsi.utils.logging2.createChildLogger
 import java.util.Random
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * [ProbingDataSender] currently supports probing via 2 methods:
@@ -58,7 +59,7 @@ class ProbingDataSender(
     private val logger = createChildLogger(parentLogger)
 
     private var rtxSupported = false
-    private val videoPayloadTypes = mutableSetOf<VideoPayloadType>()
+    private val videoPayloadTypes: MutableSet<VideoPayloadType> = ConcurrentHashMap.newKeySet()
     private var localVideoSsrc: Long? = null
 
     // Stats
