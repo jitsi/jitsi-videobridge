5ed5c6179c4d64a6620511ed954d18e1a5297682
Boris Grozev <boris@jitsi.org>
2022-06-02 12:51:12 -0500

    Move RelayConfigTest to the correct package.

--- jvb/src/test/kotlin/org/jitsi/videobridge/relay/RelayConfigTest.kt
+++ jvb/src/test/kotlin/org/jitsi/videobridge/relay/RelayConfigTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jitsi.videobridge.octo.config
+package org.jitsi.videobridge.relay
 
 import io.kotest.matchers.shouldBe
 import org.jitsi.ConfigTest
