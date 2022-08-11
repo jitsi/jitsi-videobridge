01deaca4f250199576ccde12c53021d75f88912c
Alexandre Fonseca <4717855+alexcsf@users.noreply.github.com>
2022-08-11 12:25:26 +0100

    Port some more metrics to MetricsContainer.

--- jvb/src/main/java/org/jitsi/videobridge/EndpointMessageTransport.java
+++ jvb/src/main/java/org/jitsi/videobridge/EndpointMessageTransport.java
@@ -33,7 +33,6 @@ import java.util.concurrent.atomic.*;
 import java.util.function.*;
 import java.util.stream.*;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.jitsi.videobridge.VersionConfig.config;
 import static org.jitsi.videobridge.util.MultiStreamCompatibilityKt.endpointIdToSourceName;
 
@@ -230,7 +229,7 @@ public class EndpointMessageTransport
     private void sendMessage(DataChannel dst, BridgeChannelMessage message)
     {
         dst.sendString(message.toJson());
-        statisticsSupplier.get().totalDataChannelMessagesSent.incrementAndGet();
+        statisticsSupplier.get().dataChannelMessagesSent.inc();
     }
 
     /**
@@ -255,7 +254,7 @@ public class EndpointMessageTransport
     public void onDataChannelMessage(DataChannelMessage dataChannelMessage)
     {
         webSocketLastActive = false;
-        statisticsSupplier.get().totalDataChannelMessagesReceived.incrementAndGet();
+        statisticsSupplier.get().dataChannelMessagesReceived.inc();
 
         if (dataChannelMessage instanceof DataChannelStringMessage)
         {
