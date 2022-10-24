506b1e8c4adbcef1c4f2835a82d41aa30ec48e9c
J <84336185+jqdrqgnq@users.noreply.github.com>
2022-10-24 16:22:21 -0400

    Add some stats for SSRC rewriting. (#1954)
    May be useful for tuning the limits.
    
    Co-authored-by: James A <jqdrqgnq@users.noreply.github.com>

--- jvb/src/main/kotlin/org/jitsi/videobridge/Endpoint.kt
+++ jvb/src/main/kotlin/org/jitsi/videobridge/Endpoint.kt
@@ -1115,6 +1115,10 @@ class Endpoint @JvmOverloads constructor(
             put("acceptAudio", acceptAudio)
             put("acceptVideo", acceptVideo)
             put("messageTransport", messageTransport.debugState)
+            if (doSsrcRewriting) {
+                put("audioSsrcs", audioSsrcs.getDebugState())
+                put("videoSsrcs", videoSsrcs.getDebugState())
+            }
         }
     }
 
