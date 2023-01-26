0aa0b1f672f1e92d0382f16145888746aa5cc6a2
Nils Ohlmeier <nils-ohlmeier@users.noreply.github.com>
2023-01-26 08:42:36 -0800

    Cleanup DTLS 1.0 leftovers (#1983)
    * Removed DTLS 1.0 from client and server
    
    * added more ciphers
    
    * updated cipher list based on Mozilla SSL configurator
    
    * Make dtls cipher suites configurable.
    
    * Use cipher suites from config.
    
    * squash: Remove unused imports.
    
    * Require non-empty cipher suites.
    
    * squash: Fix ktlint.
    
    * Fix typo.
    
    * Better exception
    
    * commented out AES 256 ciphers, because they are not supported yet
    
    * exit early on mis-configured dtls ciphers
    
    * don't restart after configuration error
    
    Co-authored-by: Boris Grozev <boris@jitsi.org>

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/DtlsConfig.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/DtlsConfig.kt
@@ -15,7 +15,9 @@
  */
 package org.jitsi.nlj.dtls
 
+import org.bouncycastle.tls.CipherSuite
 import org.jitsi.config.JitsiConfig
+import org.jitsi.metaconfig.ConfigException
 import org.jitsi.metaconfig.config
 import java.time.Duration
 
@@ -23,4 +25,24 @@ class DtlsConfig {
     val handshakeTimeout: Duration by config {
         "jmt.dtls.handshake-timeout".from(JitsiConfig.newConfig)
     }
+
+    val cipherSuites: List<Int> by config {
+        "jmt.dtls.cipher-suites".from(JitsiConfig.newConfig).convertFrom<List<String>> { list ->
+            val ciphers = list.map { it.toBcCipherSuite() }
+            if (ciphers.isEmpty()) {
+                throw ConfigException.UnableToRetrieve.ConditionNotMet("cipher-suites must not be empty")
+            }
+            ciphers
+        }
+    }
+
+    companion object {
+        val config = DtlsConfig()
+    }
+}
+
+private fun String.toBcCipherSuite(): Int = try {
+    CipherSuite::class.java.getDeclaredField(this).getInt(null)
+} catch (e: Exception) {
+    throw ConfigException.UnableToRetrieve.ConditionNotMet("Value is not a valid BouncyCastle cipher suite name: $this")
 }
