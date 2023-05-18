b15c1b6aa64e7e8903cd32d08570f1247714615a
Jonathan Lennox <jonathan.lennox@8x8.com>
2023-05-18 18:17:17 -0400

    Kotlin warning fix.

--- rtp/src/test/kotlin/org/jitsi/rtp/rtp/header_extensions/Av1DependencyDescriptorHeaderExtensionTest.kt
+++ rtp/src/test/kotlin/org/jitsi/rtp/rtp/header_extensions/Av1DependencyDescriptorHeaderExtensionTest.kt
@@ -68,8 +68,8 @@ class Av1DependencyDescriptorHeaderExtensionTest : ShouldSpec() {
                     val structure = ld1.newTemplateDependencyStructure
                     structure shouldNotBe null
                     structure!!.decodeTargetCount shouldBe 1
-                    structure!!.maxTemporalId shouldBe 0
-                    structure!!.maxSpatialId shouldBe 0
+                    structure.maxTemporalId shouldBe 0
+                    structure.maxSpatialId shouldBe 0
                 }
                 should("be parseable statelessly") {
                     val ld1s = ld1r.parseStateless()
