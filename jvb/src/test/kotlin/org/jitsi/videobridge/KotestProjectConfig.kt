90b6a54fa745a0e3bdbd83e034e20a08954fdb01
Jonathan Lennox <jonathan.lennox@8x8.com>
2023-09-27 12:56:42 -0400

    Update Kotlin to 1.9.10, and related Maven packages.

--- jvb/src/test/kotlin/org/jitsi/videobridge/KotestProjectConfig.kt
+++ jvb/src/test/kotlin/org/jitsi/videobridge/KotestProjectConfig.kt
@@ -21,7 +21,7 @@ import org.jitsi.metaconfig.MetaconfigSettings
 import org.jitsi.videobridge.metrics.VideobridgeMetricsContainer
 
 class KotestProjectConfig : AbstractProjectConfig() {
-    override fun beforeAll() = super.beforeAll().also {
+    override suspend fun beforeProject() = super.beforeProject().also {
         // The only purpose of config caching is performance. We always want caching disabled in tests (so we can
         // freely modify the config without affecting other tests executing afterwards).
         MetaconfigSettings.cacheEnabled = false
