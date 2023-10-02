927565cc15722fbefe8393cede8f1e868179e9fa
Jonathan Lennox <jonathan.lennox@8x8.com>
2023-10-02 11:43:37 -0400

    Implement ProbeBitrateEstimator.

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/rtp/bandwidthestimation2/ProbeBitrateEstimator.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/rtp/bandwidthestimation2/ProbeBitrateEstimator.kt
@@ -17,15 +17,174 @@
 package org.jitsi.nlj.rtp.bandwidthestimation2
 
 import org.jitsi.nlj.util.Bandwidth
+import org.jitsi.nlj.util.DataSize
+import org.jitsi.nlj.util.bytes
+import org.jitsi.nlj.util.min
+import org.jitsi.nlj.util.per
+import org.jitsi.utils.logging2.createLogger
+import org.jitsi.utils.secs
+import java.time.Duration
+import java.time.Instant
+import java.util.*
 
+/**
+ * Basic implementation to estimate bitrate of probes.
+ *
+ * Based on WebRTC modules/congestion_controller/goog_cc/probe_bitrate_estimator.{h,cc} in
+ * WebRTC 8284f2b4e8670529d039a8b6c73ec5f1d760bd21.
+ */
 class ProbeBitrateEstimator {
+    // TODO: pass parent logger in so we have log contexts
+    private val logger = createLogger()
+
+    private val clusters = TreeMap<Int, AggregatedCluster>()
+
+    private var estimatedDataRate: Bandwidth? = null
+
     fun handleProbeAndEstimateBitrate(packetFeedback: PacketResult): Bandwidth? {
-        /* TODO */
-        return null
+        val clusterId = packetFeedback.sentPacket.pacingInfo.probeClusterId
+        check(clusterId != PacedPacketInfo.kNotAProbe)
+
+        eraseOldClusters(packetFeedback.receiveTime)
+
+        val cluster = clusters.getOrPut(clusterId) { AggregatedCluster() }
+
+        if (packetFeedback.sentPacket.sendTime < cluster.firstSend) {
+            cluster.firstSend = packetFeedback.sentPacket.sendTime
+        }
+        if (packetFeedback.sentPacket.sendTime > cluster.lastSend) {
+            cluster.lastSend = packetFeedback.sentPacket.sendTime
+            cluster.sizeLastSend = packetFeedback.sentPacket.size
+        }
+        if (packetFeedback.receiveTime < cluster.firstReceive) {
+            cluster.firstReceive = packetFeedback.receiveTime
+            cluster.sizeFirstReceive = packetFeedback.sentPacket.size
+        }
+        if (packetFeedback.receiveTime > cluster.lastReceive) {
+            cluster.lastReceive = packetFeedback.receiveTime
+        }
+        cluster.sizeTotal += packetFeedback.sentPacket.size
+        cluster.numProbes += 1
+
+        check(packetFeedback.sentPacket.pacingInfo.probeClusterMinProbes > 0)
+        check(packetFeedback.sentPacket.pacingInfo.probeClusterMinBytes > 0)
+
+        val minProbes = packetFeedback.sentPacket.pacingInfo.probeClusterMinProbes *
+            kMinReceivedProbesRatio
+        val minSize = packetFeedback.sentPacket.pacingInfo.probeClusterMinBytes.bytes *
+            kMinReceivedBytesRatio
+        if (cluster.numProbes < minProbes || cluster.sizeTotal < minSize) {
+            return null
+        }
+
+        val sendInterval = Duration.between(cluster.firstSend, cluster.lastSend)
+        val receiveInterval = Duration.between(cluster.firstReceive, cluster.lastReceive)
+        if (sendInterval <= Duration.ZERO || sendInterval > kMaxProbeInterval ||
+            receiveInterval <= Duration.ZERO ||
+            receiveInterval > kMaxProbeInterval
+        ) {
+            logger.info(
+                "Probing unsuccessful, invalid send/receive interval " +
+                    "[cluster id: $clusterId] [sendInterval: $sendInterval] " +
+                    "[receive interval: $receiveInterval]"
+            )
+            return null
+        }
+        // Since the `send_interval` does not include the time it takes to actually
+        // send the last packet the size of the last sent packet should not be
+        // included when calculating the send bitrate.
+        check(cluster.sizeTotal >= cluster.sizeLastSend)
+        val sendSize = cluster.sizeTotal - cluster.sizeLastSend
+        val sendRate = sendSize.per(sendInterval)
+
+        // Since the `receive_interval` does not include the time it takes to
+        // actually receive the first packet the size of the first received packet
+        // should not be included when calculating the receive bitrate.
+        check(cluster.sizeTotal >= cluster.sizeFirstReceive)
+        val receiveSize = cluster.sizeTotal - cluster.sizeFirstReceive
+        val receiveRate = receiveSize.per(receiveInterval)
+
+        val ratio = receiveRate / sendRate
+        if (ratio > kMaxValidRatio) {
+            logger.info(
+                "Probing unsuccessful, receive/send ratio too high " +
+                    "[cluster id: $clusterId] [send: $sendSize/$sendInterval = $sendRate] " +
+                    "[receive: $receiveSize/$receiveInterval = $receiveRate] " +
+                    "[ratio: $receiveRate / $sendRate = $ratio > kMaxValidRatio ($kMaxValidRatio)]"
+            )
+            return null
+        }
+        logger.info(
+            "Probing successful [cluster id: $clusterId] " +
+                "[send: $sendSize / $sendInterval = $sendRate]" +
+                "[receive: $receiveSize / $receiveInterval = $receiveRate]"
+        )
+
+        var res = min(sendRate, receiveRate)
+        // If we're receiving at significantly lower bitrate than we were sending at,
+        // it suggests that we've found the true capacity of the link. In this case,
+        // set the target bitrate slightly lower to not immediately overuse.
+        if (receiveRate < sendRate * kMinRatioForUnsaturatedLink) {
+            check(sendRate > receiveRate)
+            res = receiveRate * kTargetUtilizationFraction
+        }
+        /* TODO: timeseries log */
+        estimatedDataRate = res
+        return estimatedDataRate
     }
 
     fun fetchAndResetLastEstimatedBitrate(): Bandwidth? {
-        /* TODO */
-        return null
+        val estimatedDataRate = this.estimatedDataRate
+        this.estimatedDataRate = null
+        return estimatedDataRate
+    }
+
+    private fun eraseOldClusters(timestamp: Instant) {
+        clusters.entries.removeIf {
+            it.value.lastReceive + kMaxClusterHistory < timestamp
+        }
+    }
+
+    private data class AggregatedCluster(
+        var numProbes: Int = 0,
+        var firstSend: Instant = Instant.MAX,
+        var lastSend: Instant = Instant.MIN,
+        var firstReceive: Instant = Instant.MAX,
+        var lastReceive: Instant = Instant.MIN,
+        var sizeLastSend: DataSize = DataSize.ZERO,
+        var sizeFirstReceive: DataSize = DataSize.ZERO,
+        var sizeTotal: DataSize = DataSize.ZERO
+    )
+
+    companion object {
+        /** The minumum number of probes we need to receive feedback about in percent
+         * in order to have a valid estimate. */
+        const val kMinReceivedProbesRatio = 0.80
+
+        /** The minumum number of bytes we need to receive feedback about in percent
+         * in order to have a valid estimate. */
+        const val kMinReceivedBytesRatio = 0.80
+
+        /** The maximum |receive rate| / |send rate| ratio for a valid estimate. */
+        const val kMaxValidRatio = 2.0f
+
+        /** The minimum |receive rate| / |send rate| ratio assuming that the link is
+         * not saturated, i.e. we assume that we will receive at least
+         * kMinRatioForUnsaturatedLink * |send rate| if |send rate| is less than the
+         * link capacity. */
+        const val kMinRatioForUnsaturatedLink = 0.9
+
+        /* The target utilization of the link. If we know true link capacity
+         * we'd like to send at 95% of that rate. */
+        const val kTargetUtilizationFraction = 0.95
+
+        /* The maximum time period over which the cluster history is retained.
+         * This is also the maximum time period beyond which a probing burst is not
+         * expected to last. */
+        val kMaxClusterHistory = 1.secs
+
+        /* The maximum time interval between first and the last probe on a cluster
+         * on the sender side as well as the receive side. */
+        val kMaxProbeInterval = 1.secs
     }
 }
