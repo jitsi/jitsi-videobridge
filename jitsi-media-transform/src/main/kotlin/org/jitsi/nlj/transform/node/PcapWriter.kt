b47039dacf1d3eba6f3372fdd9829ac10da26613
Danail Kirov <dkirov@bloomberg.net>
2023-11-10 15:39:06 -0800

    Fixed default file path

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/transform/node/PcapWriter.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/transform/node/PcapWriter.kt
@@ -36,7 +36,7 @@ import java.util.Random
 
 class PcapWriter(
     parentLogger: Logger,
-    filePath: String = "/tmp/${Random().nextLong()}.pcap}"
+    filePath: String = "/tmp/${Random().nextLong()}.pcap"
 ) : ObserverNode("PCAP writer") {
     private val logger = createChildLogger(parentLogger)
     private val lazyHandle = lazy {
