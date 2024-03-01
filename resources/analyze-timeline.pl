036534e14ee93ae25f2dae63822a14d1b0725d2b
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-03-01 16:42:46 -0500

    Update analyze-timeline.pl for timeline change to Duration.

--- resources/analyze-timeline.pl
+++ resources/analyze-timeline.pl
@@ -9,7 +9,7 @@ my @stat_names;
 while (<>) {
     if (/Reference time: /) {
         my $prev_time;
-        while (/; \(([^)]*), ([0-9]*)/g) {
+        while (/; \(([^)]*), PT([0-9.]*)/g) {
             my $name = $1;
             my $time = $2;
 
@@ -30,13 +30,13 @@ while (<>) {
                 $stats{$name} = Statistics::Descriptive::Full->new();
                 push(@stat_names, $name);
             }
-            $stats{$name}->add_data($delta_time);
+            $stats{$name}->add_data($delta_time * 1e3);
         }
     }
 }
 
 for my $name (@stat_names) {
     my $s = $stats{$name};
-    printf("%s: min %d ms, mean %d ms, median %d ms, 90%% %d ms, 99%% %d ms, max %d ms\n",
+    printf("%s: min %.3f ms, mean %.3f ms, median %.3f ms, 90%% %.3f ms, 99%% %.3f ms, max %.3f ms\n",
            $name, $s->min(), $s->mean(), $s->median(), scalar($s->percentile(90)), scalar($s->percentile(99)), $s->max());
 }
