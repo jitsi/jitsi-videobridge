9ac286cc1512afff7381d50c7d5dd50fd2c9aaef
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-12-06 11:59:26 -0500

    Support "no signature" signers on local X.509 certificates.
    As defined in draft-davidben-x509-alg-none.

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/DtlsConfig.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/DtlsConfig.kt
@@ -54,6 +54,10 @@ class DtlsConfig private constructor() {
         }
     }
 
+    val useNoSignatureSigner: Boolean by config {
+        "jmt.dtls.use-no-signature-signer".from(JitsiConfig.newConfig)
+    }
+
     companion object {
         val config = DtlsConfig()
     }
