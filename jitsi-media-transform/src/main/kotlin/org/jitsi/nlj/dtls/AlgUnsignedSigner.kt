80c01b17526468fb8aee377f989fbc7fdda36256
Jonathan Lennox <jonathan.lennox@8x8.com>
2025-05-29 11:55:33 -0400

    Update to draft-ietf-lamps-x509-alg-none-05.

--- jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/AlgUnsignedSigner.kt
+++ jitsi-media-transform/src/main/kotlin/org/jitsi/nlj/dtls/AlgUnsignedSigner.kt
@@ -1,18 +1,17 @@
 package org.jitsi.nlj.dtls
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier
-import org.bouncycastle.asn1.DERNull
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier
 import org.bouncycastle.operator.ContentSigner
 import java.io.OutputStream
 
 /**
- * A "signing" algorithm which produces an empty signature.  Based on
- * draft-davidben-x509-alg-none.
+ * A "signing" algorithm which produces an empty signature.  Defined by
+ * draft-ietf-lamps-x509-alg-none, current implementation based on version -05.
  * Should be treated as an unknown algorithm by all recipients, so any attempt to
  * validate it will return false.
  */
-class NoSignatureSigner : ContentSigner {
+class AlgUnsignedSigner : ContentSigner {
     override fun getAlgorithmIdentifier(): AlgorithmIdentifier {
         return identifier
     }
@@ -31,17 +30,9 @@ class NoSignatureSigner : ContentSigner {
     companion object {
         private val identifier = AlgorithmIdentifier(
             /*
-             id-pkix OBJECT IDENTIFIER  ::= { iso(1) identified-organization(3)
-                  dod(6) internet(1) security(5) mechanisms(5) pkix(7) }
-
-              id-alg-noSignature OBJECT IDENTIFIER ::= {id-pkix id-alg(6) 2}
+                id-alg-unsigned OBJECT IDENTIFIER ::= {1 3 6 1 5 5 7 6 36}
              */
-            ASN1ObjectIdentifier("1.3.6.1.5.5.7.6.2"),
-            /*
-            The parameters for id-alg-noSignature MUST be present
-               and MUST be encoded as NULL.
-             */
-            DERNull.INSTANCE
+            ASN1ObjectIdentifier("1.3.6.1.5.5.7.6.36")
         )
     }
 }
